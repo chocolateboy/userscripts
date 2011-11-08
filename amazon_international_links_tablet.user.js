85fdb4dfe0597352e56e5c4c524ebde6b84e3360
chocolateboy <chocolate@cpan.org>
2011-11-08 21:43:32 +0000

    update to work with latest cross-site Amazon design

--- amazon_international_links_tablet.user.js
+++ amazon_international_links_tablet.user.js
@@ -3,28 +3,26 @@
 // @author        chocolateboy
 // @copyright     chocolateboy
 // @namespace     https://github.com/chocolateboy/userscripts
-// @version       0.44
+// @version       0.50
 // @license       GPL: http://www.gnu.org/copyleft/gpl.html
 // @description   Add international links to Amazon product pages
+// @include       http://www.amazon.at/*
 // @include       http://www.amazon.ca/*
 // @include       http://www.amazon.cn/*
 // @include       http://www.amazon.co.jp/*
 // @include       http://www.amazon.com/*
 // @include       http://www.amazon.co.uk/*
 // @include       http://www.amazon.de/*
-// @include       http://www.amazon.es/*
 // @include       http://www.amazon.fr/*
-// @include       http://www.amazon.it/*
+// @include       https://www.amazon.at/*
 // @include       https://www.amazon.ca/*
 // @include       https://www.amazon.cn/*
 // @include       https://www.amazon.co.jp/*
 // @include       https://www.amazon.com/*
 // @include       https://www.amazon.co.uk/*
 // @include       https://www.amazon.de/*
-// @include       https://www.amazon.es/*
 // @include       https://www.amazon.fr/*
-// @include       https://www.amazon.it/*
-// @require       https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js
+// @require       https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js
 // @require       https://raw.github.com/sizzlemctwizzle/GM_config/master/gm_config.js
 // @require       https://sprintf.googlecode.com/files/sprintf-0.7-beta1.js
 // @require       http://documentcloud.github.com/underscore/underscore-min.js
@@ -33,9 +31,9 @@
 /*
  * @requires:
  *
- * jQuery 1.6.4
+ * jQuery 1.6.2
  *
- *     https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.js
+ *     https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.js
  *
  * GM_config
  *
@@ -61,7 +59,7 @@
 
 // these are all initialized lazily (hence "var" instead of "const")
 // XXX this is pointless if thousands of lines of jQuery, sprintf &c. are executed for every Amazon page
-var $ASIN, $CROSS_SITE_LINK_CLASS, $CURRENT_TLD, $LINK, $LINKS, $PROTOCOL, $SEPARATOR, $SITES;
+var $ASIN, $CROSS_SHOP_LINKS, $CURRENT_TLD, $LINKS, $PROTOCOL, $SITES;
 
 // convenience function to reduce the verbosity of Underscore.js chaining
 // see: http://github.com/documentcloud/underscore/issues/issue/37
@@ -74,19 +72,16 @@ function initializeConstants($asin) {
     var $location = document.location;
 
     $ASIN = $asin; // the unique Amazon identifier for this product
-    $CROSS_SITE_LINK_CLASS = 'navCrossshopYALink'; // the CSS class for cross-site links
     $CURRENT_TLD = $location.hostname.substr('www.amazon.'.length); // one of the 8 current Amazon TLDs
-    $LINK = $('a.' + $CROSS_SITE_LINK_CLASS).eq(-2); // the penultimate Amazon cross-site link e.g. "Your Account"
-    $LINKS = []; // an array of our added elements - jQuery objects representing alternating links and separators
+    $CROSS_SHOP_LINKS = $('#nav-cross-shop-links'); // the cross-site/shop links container
+    $LINKS = []; // an array of our added elements - jQuery objects representing <li><a></a></li> elements
     $PROTOCOL = $location.protocol; // http: or https:
-    $SEPARATOR = $LINK.next(); // a span with a spaced vertical bar
     $SITES = { // a map from the Amazon TLD to the corresponding two-letter country code
+        'at'    : 'AT',
         'ca'    : 'CA',
         'cn'    : 'CN',
         'de'    : 'DE',
-        'es'    : 'ES',
         'fr'    : 'FR',
-        'it'    : 'IT',
         'co.jp' : 'JP',
         'co.uk' : 'UK', // technically (shmecnically), this should be GB: http://en.wikipedia.org/wiki/ISO_3166-1_alpha-2
         'com'   : 'US'
@@ -138,10 +133,10 @@ function getConfiguredSites() {
 // remove all added links from the DOM and clear the array referencing them
 function removeLinks() {
     _($LINKS).each(function($el) { $el.remove() });
-    $LINKS.length = 0; // clear the array of links and separators
+    $LINKS.length = 0; // clear the array of links
 }
 
-// populate an array of links and display them by prepending them to the body of the cross-site navigation bar
+// populate an array of links and display them by appending them to the body of the cross-site navigation bar
 function addLinks() {
     var $sites = getConfiguredSites();
 
@@ -153,28 +148,28 @@ function addLinks() {
             var $html;
 
             if ($tld == $CURRENT_TLD) {
-                $html = sprintf('<strong title="amazon.%s">%s</strong>', $tld, $country);
+                $html = sprintf('<li class="nav-xs-link"><strong title="amazon.%s">%s</strong></li>', $tld, $country);
             } else {
                 $html = sprintf(
-                    '<a href="%s//www.amazon.%s/dp/%s" class="%s" title="amazon.%2$s">%s</a>',
-                    $PROTOCOL, $tld, $ASIN, $CROSS_SITE_LINK_CLASS, $country
+                    '<li class="nav-xs-link"><a href="%s//www.amazon.%s/dp/%s" title="amazon.%2$s">%s</a></li>',
+                    $PROTOCOL, $tld, $ASIN, $country
                 );
             }
 
-            $LINKS.push($($html), $SEPARATOR.clone());
+            $LINKS.push($($html));
         });
 
-        // prepend the cross-site links to the "Your Account" link
-        $LINK.before.apply($LINK, $LINKS);
+        // append the cross-site links to the body of the nav-cross-shop-links <ul> element
+        $CROSS_SHOP_LINKS.append.apply($CROSS_SHOP_LINKS, $LINKS);
     }
 }
 
 /*********************** Main ********************************/
 
-var $temp = $('input#ASIN, input[name="ASIN"], input[name="ASIN.0"]');
+var $asin = $('#ASIN');
 
-if ($temp.length) {
-    initializeConstants($temp.val());
+if ($asin.length) {
+    initializeConstants($asin.val());
     initializeConfig();
     GM_registerMenuCommand('Configure Amazon International Links', showConfig);
     addLinks();
