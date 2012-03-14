9ac8568a8fe1ce9c1266a806faa97512f5b81dd9
chocolateboy <chocolate@cpan.org>
2012-03-14 19:36:36 +0000

    RT: add HTTPS support

--- more_tomatoes.user.js
+++ more_tomatoes.user.js
@@ -3,17 +3,19 @@
 // @namespace   https://github.com/chocolateboy/userscripts
 // @description Automatically show the full "Movie Info" plot synopsis on Rotten Tomatoes
 // @author      chocolateboy
-// @version     0.0.1
+// @version     0.1.0
 // @license     GPL: http://www.gnu.org/copyleft/gpl.html
 // @include     http://rottentomatoes.com/m/*
 // @include     http://*.rottentomatoes.com/m/*
-// @require     https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.js
+// @include     https://rottentomatoes.com/m/*
+// @include     https://*.rottentomatoes.com/m/*
+// @require     https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.js
 // ==/UserScript==
 
 /*
- * jQuery 1.6.4
+ * jQuery 1.7.1
  *
- *     https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.js
+ *     https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.js
  */
 
 $('#showMoreSynopsis').get(0).click();
