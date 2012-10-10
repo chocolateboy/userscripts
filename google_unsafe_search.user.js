1f8edf182a44b35802e0f8f2a6fe744c6f9945f7
chocolateboy <chocolate@cpan.org>
2012-10-10 14:30:11 +0100

    add lightweight Google unsafe search script

---
+++ google_unsafe_search.user.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name          Google Unsafe Search
+// @description   Disable SafeSearch on Google Image searches
+// @author        chocolateboy
+// @copyright     chocolateboy
+// @namespace     https://github.com/chocolateboy/userscripts
+// @version       0.0.1
+// @license       GPL: http://www.gnu.org/copyleft/gpl.html
+// @include       http://www.google.tld/search?*tbm=isch*
+// @include       http://images.google.tld/search?*tbm=isch*
+// @include       https://www.google.tld/search?*tbm=isch*
+// @include       https://encrypted.google.tld/search?*tbm=isch*
+// @run-at        document-start
+// ==/UserScript==
+
+// work around Scriptish (@run-at?) bug that causes this script
+// to be run for a matched page and then triggered again for an
+// empty location (with the @run-at directive removed, the location
+// is about:blank)
+if (location.search) {
+    var oldQuery = location.search.substring(1);
+    var newQuery = oldQuery;
+
+    newQuery = newQuery.replace(/\bsafe=(?:\w+)?/g, 'safe=off');
+
+    if (!newQuery.match(/\bsafe=off\b/)) {
+        newQuery = 'safe=off&' + newQuery;
+    }
+
+    if (newQuery != oldQuery) {
+        location.replace(
+            location.protocol +
+            '//' +
+            location.hostname +
+            location.pathname +
+            '?' +
+            newQuery +
+            location.hash
+        );
+    }
+}
