a7d8bbf9627e7e86953a4e8595d71d0908053acf
chocolateboy <chocolate@cpan.org>
2013-07-10 05:34:48 +0100

    fix article selector

--- read_reddit_yellow_reddit.user.js
+++ read_reddit_yellow_reddit.user.js
@@ -4,7 +4,7 @@
 // @author        chocolateboy
 // @copyright     chocolateboy
 // @namespace     https://github.com/chocolateboy/userscripts
-// @version       0.0.3
+// @version       0.0.4
 // @license       GPL: http://www.gnu.org/copyleft/gpl.html
 // @include       http://www.reddit.com/
 // @require       https://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.js
@@ -21,7 +21,7 @@
  */
 
 const HIGHLIGHT_COLOR = '#FFFF00';
-const KEY = 'Reddit_Front_Page';
+const KEY = 'article_ids';
 
 var old_array = [], new_array = [];
 var old_string = GM_getValue(KEY, '');
@@ -30,7 +30,7 @@ if (old_string.indexOf(',') != -1) {
     old_array = old_string.split(',');
 }
 
-$('div.thing').each(function() {
+$('div.thing[data-fullname]').each(function() {
     var id = $(this).attr('data-fullname');
 
     if ($.inArray(id, old_array) == -1) {
