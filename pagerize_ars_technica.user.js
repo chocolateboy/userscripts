958bd0438857db1947ddef82066235dbb38b5310
chocolateboy <chocolate@cpan.org>
2013-10-30 21:24:28 +0000

    nitfix

--- pagerize_ars_technica.user.js
+++ pagerize_ars_technica.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name          Pagerize Ars Technica
-// @description   Mark up Ars Technica with Pager metadata
+// @description   Mark up Ars Technica with Pager Metadata
 // @author        chocolateboy
 // @copyright     chocolateboy
 // @namespace     https://github.com/chocolateboy/userscripts
