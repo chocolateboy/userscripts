a4e3500187c186cc075efd6fb4195922ba9936d9
chocolateboy <chocolate@cpan.org>
2014-02-17 19:31:03 +0000

    fix broken reddit custom style toggler

--- maintenance/reddit_toggle_custom_styles.user.js
+++ maintenance/reddit_toggle_custom_styles.user.js
@@ -3,63 +3,63 @@
 // @description    Enable/disable subreddit-specific CSS, flair and thumbnails
 // @author         DEADBEEF.
 // @maintainer     chocolateboy
-// @namespace      https://github.com/chocolatgeboy/userscript
+// @namespace      https://github.com/chocolateboy/userscript
 // @include        http://reddit.com/r/*
 // @include        https://reddit.com/r/*
 // @include        http://*.reddit.com/r/*
 // @include        https://*.reddit.com/r/*
-// @version        1.0.0
+// @version        1.1.0
 // @run-at         document-start
-// @grant          GM_getValue
-// @grant          GM_setValue
+// @grant          none
 // ==/UserScript==
 
 // original: http://userscripts.org/scripts/show/109818
 
-function main() {
-    var key = 'ss-' + reddit.post_site, // post_site: subreddit ID e.g. watch_dogs
-    value = GM_getValue(key, 0);
+function ssMain() {
+    var key   = 'ss-' + reddit.post_site, // post_site: subreddit ID e.g. watch_dogs
+        value = localStorage.getItem(key) || 0;
 
     // Add buttons
     $('div.titlebox span.fancy-toggle-button').after(
-        '<span style="" class="fancy-toggle-button toggle"><a tabindex="100" href="javascript:void(0)" style="margin-left:-5px" class="custom-css active remove">css</a></span>' +
-        '<span style="" class="fancy-toggle-button toggle"><a tabindex="100" href="javascript:void(0)" style="margin-left:-5px" class="custom-flair active remove">flair</a></span>' +
-        '<span style="" class="fancy-toggle-button toggle"><a tabindex="100" href="javascript:void(0)" style="margin-left:-5px" class="custom-thumbs active remove">thumbs</a></span><div />'
+        '<span style="" class="fancy-toggle-button toggle"><a tabindex="100" href="javascript:void(0)" style="margin-left:-5px" class="custom-css active add remove">css</a></span>' +
+        '<span style="" class="fancy-toggle-button toggle"><a tabindex="100" href="javascript:void(0)" style="margin-left:-5px" class="custom-flair active add remove">flair</a></span>' +
+        '<span style="" class="fancy-toggle-button toggle"><a tabindex="100" href="javascript:void(0)" style="margin-left:-5px" class="custom-thumbs active add remove">thumbs</a></span><div />'
     );
 
     // Add button actions
     $('a.custom-css').click(function() {
-        value ^= 1
+        value ^= 1;
+        localStorage.setItem(key, value);
+        set();
     });
 
     $('a.custom-flair').click(function() {
-        value ^= 2
+        value ^= 2;
+        localStorage.setItem(key, value);
+        set();
     });
 
     $('a.custom-thumbs').click(function() {
-        value ^= 4
-    });
-
-    $('a[class|="custom"]').click(function() {
+        value ^= 4;
         localStorage.setItem(key, value);
-        set()
+        set();
     });
 
     function set() {
         // Get bitmask values
-        var css    = !(value & 1),
-            flair  = !(value & 2),
-            thumbs = !(value & 4);
+        var disableCss    = !!(value & 1),
+            disableFlair  = !!(value & 2),
+            disableThumbs = !!(value & 4);
 
-        // Show/hide things.
-        $('span.flair').css('display', flair ? 'inline-block' : 'none');
-        $('a.thumbnail').css('display', thumbs ? 'block' : 'none');
-        $('link[title="applied_subreddit_stylesheet"]').prop('disabled', !css);
+        // Show/hide things
+        $('link[title="applied_subreddit_stylesheet"]').prop('disabled', disableCss);
+        $('span.flair').css('display', disableFlair ? 'none' : 'inline-block');
+        $('a.thumbnail').css('display', disableThumbs ? 'none' : 'block');
 
         // Set button state
-        $('a.custom-css').toggleClass('add', !css);
-        $('a.custom-flair').toggleClass('add', !flair);
-        $('a.custom-thumbs').toggleClass('add', !thumbs);
+        $('a.custom-css').toggleClass('add', disableCss);
+        $('a.custom-flair').toggleClass('add', disableFlair);
+        $('a.custom-thumbs').toggleClass('add', disableThumbs);
     };
 
     set();
@@ -69,24 +69,31 @@ function main() {
     window.flowwit.push(set);
 }
 
-// Add script to the page.
+// Add script to the page
 document.addEventListener('DOMContentLoaded', function(e) {
     var script = document.createElement('script');
-    script.textContent = '(' + main.toString() + ')();';
+    script.textContent = '(' + ssMain.toString() + ')();';
     document.head.appendChild(script)
 });
 
-// Add CSS as soon as the head element exists (prevents jumping).
+// Add CSS as soon as the head element exists (prevents jumping)
 (function loadCSS() {
-    if (!document.styleSheets || !document.styleSheets[1]) {
+    var stylesheet = document.querySelector('link[title="applied_subreddit_stylesheet"]');
+
+    if (!stylesheet) {
         return setTimeout(loadCSS);
     }
 
     var key   = 'ss-' + location.pathname.match(/\/r\/(\w+)/)[1],
         value = localStorage.getItem(key) || 0;
 
-    document.styleSheets[1].disabled = !!(value & 1);
-    var s = document.createElement('style');
-    s.textContent = 'span.flair{display:' + (!(value & 2) ? 'inline-block':'none') + '}a.thumbnail{display:' + (!(value & 4) ? 'block':'none') + '}';
-    document.head.appendChild(s);
+    var disableCss    = !!(value & 1),
+        disableFlair  = !!(value & 2),
+        disableThumbs = !!(value & 4);
+
+    stylesheet.disabled = disableCss;
+
+    var style = document.createElement('style');
+    style.textContent = 'span.flair { display: ' + (disableFlair ? 'none' : 'inline-block') + ' } a.thumbnail { display: ' + (disableThumbs ? 'none' : 'block') + ' }';
+    document.head.appendChild(style);
 })();
