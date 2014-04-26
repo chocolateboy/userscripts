bfd10c6c8ec391d5fd7aeeb245d39bb761c282b9
chocolateboy <chocolate@cpan.org>
2014-04-26 21:17:10 +0100

    sort.fm:
        - add support for Similar Music charts
        - make track name column sortable
        - display track number/position column and make it sortable
        - upgrade jQuery to 2.1.0

--- sort.fm.user.js
+++ sort.fm.user.js
@@ -3,136 +3,182 @@
 // @author        chocolateboy
 // @copyright     chocolateboy
 // @namespace     https://github.com/chocolateboy/userscripts
-// @version       1.3.0
+// @version       2.0.0
 // @license       GPL: http://www.gnu.org/copyleft/gpl.html
-// @description   Sort last.fm tracklists by track number, duration or number of listeners
+// @description   Sort last.fm tracklists by track number, track name, duration or number of listeners
 // @include       http://www.last.fm/music/*
 // @include       http://cn.last.fm/music/*
 // @include       http://www.lastfm.tld/music/*
-// @require       https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.js
+// @require       https://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.js
 // @grant         GM_log
 // ==/UserScript==
 
 // XXX note: the unused grant is a workaround for a Greasemonkey bug:
 // https://github.com/greasemonkey/greasemonkey/issues/1614
 
-var INITIAL_SORTED_COLUMN = 'track';
+var INITIAL_SORTED_COLUMN = 'position';
 var ASCENDING = 1, DESCENDING = -1;
+var LEXICOGRAPHICAL = 1, NUMERIC = 2;
 
 /*
  * key (string):
  *
  *     internal identifier for each column e.g. 'track', 'duration'
  *
- * value (triple):
+ * value:
  *
- *     0: CSS class name identifying the table header cell to attach the click event to
- *     1: extractor function that takes a row and returns the value of its designated column as a sortable number
- *     2: initial sort order
+ *     0: CSS class name identifying the table header cell to attach the click event to and table body cell to sort by
+ *     1: extractor function that takes a row and returns a sortable value from its designated field
+ *     2: initial sort order i.e. the sort order we should use the first time we sort the column
+ *     3: sort type: how the value extracted from a particular column is sorted: lexicographical for strings (e.g. track name),
+ *        numeric for numbers (e.g. listeners)
  */
 var COLUMN_CONFIG = {
-    'track':     [ 'subjectCell',  extract_track,     ASCENDING  ],
-    'duration':  [ 'durationCell', extract_duration,  ASCENDING  ],
-    'listeners': [ 'reachCell',    extract_listeners, DESCENDING ]
+    'position':  [ 'positionCell', extractPosition,  ASCENDING,  NUMERIC         ],
+    'track':     [ 'subjectCell',  extractTrack,     ASCENDING,  LEXICOGRAPHICAL ],
+    'duration':  [ 'durationCell', extractDuration,  ASCENDING,  NUMERIC         ],
+    'listeners': [ 'reachCell',    extractListeners, DESCENDING, NUMERIC         ]
 };
 
 // --------------------------- extractors ------------------------------
 
-function extract_track(row) {
-    return $(row).find('.positionCell').text().replace(/\D+/g, '') * 1;
+// extract the track position from a row
+function extractPosition(row, selector) {
+    return $.trim($(row).find(selector).text()) * 1;
 }
 
-function extract_duration(row) {
-    var duration = $(row).find('.durationCell').text().match(/(\d+):(\d+)/);
+// extract the track name from a row
+function extractTrack(row, selector) {
+    return $.trim($(row).find(selector).text());
+}
+
+// extract the track length from a row
+function extractDuration(row, selector) {
+    var duration = $(row).find(selector).text().match(/(\d+):(\d+)/);
     return duration[1] * 60 + duration[2] * 1;
 }
 
-function extract_listeners(row) {
-    return $(row).find('.reachCell').text().replace(/\D+/g, '') * 1;
+// extract the number of listeners from a row
+function extractListeners(row, selector) {
+    return $(row).find(selector).text().replace(/\D+/g, '') * 1;
 }
 
 // ------------------------------ helpers -------------------------------
 
-function makeCompare(extractor, order) {
-    return function(a, b) {
-        return (extractor(a) - extractor(b)) * order;
-    };
-}
+// take a column spec and an order (ascending or descending) and return a
+// function which takes two rows (a and b), extracts the values from the
+// specified column, and returns an integer which represents the ordering
+// of the rows: -1 if a should precede b, 0 if they're equal, and 1 if a
+// should follow b.
+function makeCompare(config, order) {
+    var extract = config[1];
+    var selector = '.' + config[0];
+    var sortType = config[3];
 
-function stripe (index, row) {
-    $(row).removeClass('first last odd');
+    if (sortType == LEXICOGRAPHICAL) {
+        return function(a, b) {
+            var cmp;
+            var aValue = extract(a, selector);
+            var bValue = extract(b, selector);
 
-    if ((index + 1) % 2) { // index is 0-based, so index == 0 is the first row (odd) &c.
-        $(row).addClass('odd');
+            if (aValue == bValue) {
+                cmp = 0;
+            } else {
+                cmp = aValue < bValue ? -1 : 1;
+            }
+
+            return cmp * order;
+        };
+    } else { // numeric
+        return function(a, b) {
+            return (extract(a, selector) - extract(b, selector)) * order;
+        };
     }
 }
 
 /*
  * Initialize, toggle or restore the sort order for the supplied column.
  *
- * Note: the column by which the table is initially sorted (i.e. track) is
+ * Note: the column by which the table is initially sorted (i.e. position) is
  * special-cased as it has effectively been "pre-clicked" to ascending order by last.fm.
  */
-sortOrder = function() { // create a scope for variables that are local (i.e. private) to this function
-    var lastSelectedColumn = INITIAL_SORTED_COLUMN, sort_order = {};
-    sort_order[lastSelectedColumn] = COLUMN_CONFIG[lastSelectedColumn][2];
+sortOrder = function() { // create a scope for variables that are persistent but local to this function
+    var lastSelectedColumn = INITIAL_SORTED_COLUMN;
+    var cache = {};
+
+    cache[lastSelectedColumn] = COLUMN_CONFIG[lastSelectedColumn][2];
 
     return function (column) {
-        if (!sort_order[column]) { // initialize
-            sort_order[column] = COLUMN_CONFIG[column][2]; // initial sort order
+        if (!cache[column]) { // initialize
+            cache[column] = COLUMN_CONFIG[column][2]; // initial sort order
         } else if (column === lastSelectedColumn) { // toggle
-            sort_order[column] = sort_order[column] * -1;
+            cache[column] = cache[column] * -1;
         } // else restore
 
         lastSelectedColumn = column;
-        return sort_order[column];
+        return cache[column];
     };
 }();
 
-// returns a function that sorts the table rows by the specified column
-function makeSortBy($rowContainer, column) {
-    var extractor = COLUMN_CONFIG[column][1];
-
+// return a function that sorts rows in the supplied container by the specified column
+function makeSortBy($rowContainer, column, config) {
     return function() {
         var $rows = $rowContainer.children('tr');
-        var order = sortOrder(column); // ascending (1) or descending (-1)
-        var compare = makeCompare(extractor, order); // compare(a, b) function which honours the specified order
+
+        // ascending (1) or descending (-1)
+        var order = sortOrder(column);
+
+        // compare(a, b) function (where a and b are rows) which honours
+        // the specified order (ascending or descending)
+        var compare = makeCompare(config, order);
+
         var $sortedRows = $rows.detach().sort(compare);
 
         // fix up the CSS
-        $sortedRows.each(stripe);
+        $sortedRows.removeClass('first last odd');
+        // XXX jQuery's :odd and :even selectors are 0-based, so -- confusingly --
+        // to select the odd rows, we need to use the :even selector i.e.
+        // index 0: first row (odd), index 1: second row (even) &c.
+        $sortedRows.filter(':even').addClass('odd');
         $sortedRows.first().addClass('first');
         $sortedRows.last().addClass('last');
 
-        // attach the sorted rows (TRs) to the row container (TBODY)
+        // re-attach the sorted rows (TRs) to the row container (TBODY)
         $sortedRows.appendTo($rowContainer);
     };
 }
 
 /******************************************************************************/
 
-var $table = $('table.tracklist');
+var $table = $('table.chart, table.tracklist').has('thead');
 
 if ($table.length) {
-    var $rows = $('tbody', $table).children('tr');
+    // add/populate position cells so we can reverse/restore the original sort order
+    if ($table.is('.tracklist')) {
+        $('thead tr', $table).prepend('<td class="positionCell">#</td>');
+        $('tbody tr', $table).prepend(function (index, oldHtml) {
+            return '<td class="positionCell">' + (index + 1) + '</td>'
+        });
+    } else { // .chart: position cells already exist but may be empty
+        // the header cell is always empty (&nbsp;)
+        $('thead td.positionCell', $table).text('#');
 
-    // prepend a hidden track-number cell to each row so that the original sort order
-    // can be reversed/restored by clicking the "Track" header (or its localized equivalent)
-    // note: these cells already exist (and are visible) on album tracklists (below)
-    $rows.prepend(function (index, html) {
-        var position = index + 1; // index is 0-based
-        return '<span class="positionCell" style="display: none">' + position + '</span>';
-    });
-} else {
-    $table = $('table#albumTracklist');
-}
+        // the body cells are already correctly populated for album tracklists
+        if (!$table.is('#albumTracklist')) {
+            $('tbody td.positionCell', $table).text(function (index, oldText) {
+                return index + 1;
+            });
+        }
+    }
 
-if ($table.length) {
     var $tbody = $('tbody', $table);
 
-    $.each(COLUMN_CONFIG, function(column, data) {
-        var $header = $('thead td.' + data[0], $table);
-        $header.css('cursor', 'pointer');
-        $header.click(makeSortBy($tbody, column));
+    $.each(COLUMN_CONFIG, function(column, config) {
+        var $header = $('thead td.' + config[0], $table);
+
+        if ($header.length) {
+            $header.css('cursor', 'pointer');
+            $header.click(makeSortBy($tbody, column, config));
+        }
     });
 }
