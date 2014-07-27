14f12710643c2c25c9e7810977d31ceb14934126
chocolateboy <chocolate@cpan.org>
2014-07-27 22:34:50 +0100

    Reddit New Story Highlighter enhancements
        - include top-level subreddit pages
        - cache visited article IDs for longer: 3 days -> 7 days
        - no longer clear out obsolete storage keys
        - update jQuery to 2.1.1

--- reddit_highlight_new_stories.user.js
+++ reddit_highlight_new_stories.user.js
@@ -1,34 +1,27 @@
 // ==UserScript==
-// @name          Highlight new stories on the Reddit front page
-// @description   Highlight stories promoted to the Reddit front page since the last visit
+// @name          Highlight new stories on Reddit
+// @description   Highlight new front page and subreddit stories on Reddit
 // @author        chocolateboy
 // @copyright     chocolateboy
 // @namespace     https://github.com/chocolateboy/userscripts
-// @version       0.4.1
+// @version       0.5.0
 // @license       GPL: http://www.gnu.org/copyleft/gpl.html
 // @include       http://reddit.com/
 // @include       https://reddit.com/
 // @include       http://*.reddit.com/
 // @include       https://*.reddit.com/
-// @require       https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.js
+// @include       /^http(s)?:\/\/([^.]+\.)?reddit\.com\/r\/[^\/]+\/$/
+// @require       https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.js
 // @grant         GM_deleteValue
 // @grant         GM_getValue
 // @grant         GM_setValue
 // ==/UserScript==
 
-var DAYS = 3;
+var DAYS = 7;
 var HIGHLIGHT_COLOR = '#FFFD66';
 var KEY = 'seen';
 var NOW = new Date().getTime();
-var OLD_KEYS = [ 'Reddit_homepage', 'Reddit_Front_Page', 'article_ids', 'cache' ];
 var TTL = 1000 * 60 * 60 * 24 * DAYS; // time-to-live: how long (in milliseconds) to cache IDs for
-
-// remove obsolete keys
-$.each(OLD_KEYS, function(index, value) {
-    if (GM_getValue(value)) {
-        GM_deleteValue(value);
-    }
-});
 
 // Reddit article ID -> cache expiry timestamp (epoch milliseconds)
 var seen = JSON.parse(GM_getValue(KEY, '{}'));
