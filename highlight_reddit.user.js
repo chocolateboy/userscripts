254f0f342957dde80bf3e1a9ceca47cbff11c740
chocolateboy <chocolate@cpan.org>
2014-10-29 18:07:04 +0000

    reddit highlighter: combine includes

--- highlight_reddit.user.js
+++ highlight_reddit.user.js
@@ -4,16 +4,9 @@
 // @author        chocolateboy
 // @copyright     chocolateboy
 // @namespace     https://github.com/chocolateboy/userscripts
-// @version       0.8.0
+// @version       0.9.0
 // @license       GPL: http://www.gnu.org/copyleft/gpl.html
-// @include       http://reddit.com/
-// @include       https://reddit.com/
-// @include       http://*.reddit.com/
-// @include       https://*.reddit.com/
-// @include       /^http(s)?:\/\/([^.]+\.)?reddit\.com\/r\/[^\/]+\/$/
-// @include       /^http(s)?:\/\/([^.]+\.)?reddit\.com\/r\/[^\/]+$/
-// @include       /^http(s)?:\/\/([^.]+\.)?reddit\.com\/r\/[^\/]+/(hot|new|rising|controversial|top)\/$/
-// @include       /^http(s)?:\/\/([^.]+\.)?reddit\.com\/r\/[^\/]+/(hot|new|rising|controversial|top)$/
+// @include       /^https?:\/\/([^.]+\.)?reddit\.com(\/r\/[^\/]+(/(hot|new|rising|controversial|top))?)?\/?$/
 // @require       https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.js
 // @require       https://raw.github.com/chocolateboy/userscripts/master/jquery/highlighter.js
 // @grant         GM_deleteValue
