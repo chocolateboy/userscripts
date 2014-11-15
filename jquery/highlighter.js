530fe79ad2c7132a5675da3f6772ccd301e09717
chocolateboy <chocolate@cpan.org>
2014-11-15 14:14:23 +0000

    highlighter.js: minor cleanup

--- jquery/highlighter.js
+++ jquery/highlighter.js
@@ -1,31 +1,29 @@
 // required: jQuery, GM_deleteValue, GM_getValue, GM_registerMenuCommand, GM_setValue
 
 jQuery.highlight = (function ($) {
-    var DEFAULT_ID = function ($item) { return $item.attr('id') };
+    var DEFAULT_ID     = function ($item) { return $item.attr('id') };
     var DEFAULT_TARGET = function ($item) { return $item };
-    var DEFAULT_TTL = { days: 7 };
-    var HIGHLIGHT_COLOR = '#FFFD66';
-    var KEY = 'seen';
-    var TTL = {
-        days:    24 * 60 * 60 * 1000,
-        hours:   60 * 60 * 1000,
-        minutes: 60 * 1000,
-        seconds: 1000
-    };
+    var DEFAULT_TTL    = { days: 7 };
+    var DEFAULT_COLOR  = '#FFFD66';
+    var KEY            = 'seen';
+
+    var TTL = (function (ttl) {
+        ttl.minutes = 60 * ttl.seconds;
+        ttl.hours   = 60 * ttl.minutes;
+        ttl.days    = 24 * ttl.hours;
+        ttl.weeks   = 7  * ttl.days;
+        return ttl;
+    })({ seconds: 1000 });
 
     // register this early so data can be cleared even if there's an error
     var commandName = GM_info.script.name + ': clear data';
     GM_registerMenuCommand(commandName, function () { GM_deleteValue(KEY) });
 
     function ttlToMilliseconds(ttl) {
-        var ms = 0;
+        var ms = 0, key;
 
-        for (var key in TTL) {
-            var value = ttl[key];
-
-            if (value) {
-                ms += value * TTL[key];
-            }
+        for (key in ttl) {
+            ms += ttl[key] * (TTL[key] || 0);
         }
 
         return ms;
@@ -45,7 +43,7 @@ jQuery.highlight = (function ($) {
         // time-to-live: how long (in milliseconds) to cache IDs for
         var ttl = ttlToMilliseconds(options.ttl || DEFAULT_TTL);
         // the background color of the target element(s)
-        var color = options.color || HIGHLIGHT_COLOR;
+        var color = options.color || DEFAULT_COLOR;
         // the current date/time in epoch milliseconds
         var now = new Date().getTime();
 
