50aa5a04c29ff8ca83d5ca62353c80d0923ee2f8
chocolateboy <chocolate@cpan.org>
2015-12-08 04:19:06 +0000

    fix More Tomatoes

--- more_tomatoes.user.js
+++ more_tomatoes.user.js
@@ -3,26 +3,13 @@
 // @namespace   https://github.com/chocolateboy/userscripts
 // @description Automatically show the full "Movie Info" plot synopsis on Rotten Tomatoes
 // @author      chocolateboy
-// @version     0.5.0
+// @version     0.6.0
 // @license     GPL: http://www.gnu.org/copyleft/gpl.html
 // @include     http://rottentomatoes.com/m/*
 // @include     http://*.rottentomatoes.com/m/*
 // @include     https://rottentomatoes.com/m/*
 // @include     https://*.rottentomatoes.com/m/*
-// @require     https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.js
-// @grant       GM_log
+// @grant       none
 // ==/UserScript==
 
-// XXX note: the unused grant is a workaround for a Greasemonkey bug:
-// https://github.com/greasemonkey/greasemonkey/issues/1614
-
-function expand_synopsis () {
-    var link = $('#showMoreSynopsis').get(0);
-
-    if (link) {
-        link.click();
-    }
-}
-
-// execute as late as possible - needed for the latest version of the site
-$(window).on('load', expand_synopsis);
+document.getElementById('movieSynopsis').removeAttribute('id');
