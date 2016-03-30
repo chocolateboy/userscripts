26d32ba49c59c5874c97f0b9bf930e7e0769610b
chocolateboy <chocolate@cpan.org>
2016-03-30 12:36:41 +0100

    Digg Highlighter: don't highlight sponsored articles + bump jQuery

--- highlight_digg.user.js
+++ highlight_digg.user.js
@@ -4,11 +4,11 @@
 // @author        chocolateboy
 // @copyright     chocolateboy
 // @namespace     https://github.com/chocolateboy/userscripts
-// @version       0.3.1
+// @version       0.4.0
 // @license       GPL: http://www.gnu.org/copyleft/gpl.html
 // @include       http://digg.com/
 // @include       https://digg.com/
-// @require       https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.js
+// @require       https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.js
 // @require       https://raw.github.com/chocolateboy/userscripts/master/jquery/highlighter.js
 // @grant         GM_deleteValue
 // @grant         GM_getValue
@@ -18,7 +18,11 @@
 
 $.highlight({
     ttl:    { days: 4 },
-    item:   'article[data-content-id]',
+    item:   function () {
+        return $('article[data-content-id]')
+            .not('[data-primary-tag-slug="apps-we-digg"]')
+            .not('[data-primary-tag-slug="digg-store"]')
+    },
     target: 'a.digg-story__title-link',
     id:     'data-content-id'
 });
