3701e18d02bd33232334b7939e91464aa9fc1cbe
chocolateboy <chocolate@cpan.org>
2016-04-28 18:12:58 +0100

    doc tweak

--- reddit_toggle_custom_css.user.js
+++ reddit_toggle_custom_css.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           Reddit - Toggle Custom CSS
-// @description    Enable/disable subreddit-specific CSS
+// @description    Enable/disable subreddit-specific CSS via a userscript command
 // @author         chocolateboy
 // @namespace      https://github.com/chocolateboy/userscripts
 // @include        http://reddit.com/r/*
