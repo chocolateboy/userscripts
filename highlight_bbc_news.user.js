6c91cf475688acea9a1b1863940727d3767434d6
chocolateboy <chocolate@cpan.org>
2016-08-11 00:36:55 +0100

    BBC highlighter fixes
    - fix white headlines
    - fix picture gallery headlines
    - update jQuery to 2.2.4

--- highlight_bbc_news.user.js
+++ highlight_bbc_news.user.js
@@ -4,20 +4,28 @@
 // @author        chocolateboy
 // @copyright     chocolateboy
 // @namespace     https://github.com/chocolateboy/userscripts
-// @version       0.3.0
+// @version       0.4.0
 // @license       GPL: http://www.gnu.org/copyleft/gpl.html
 // @include       http://www.bbc.co.uk/news
 // @include       http://www.bbc.com/news
-// @require       https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.js
-// @require       https://raw.github.com/chocolateboy/userscripts/master/jquery/highlighter.js
+// @require       https://code.jquery.com/jquery-2.2.4.min.js
+// @require       https://raw.githubusercontent.com/eclecto/jQuery-onMutate/master/jquery.onmutate.min.js
+// @require       https://raw.github.com/chocolateboy/userscripts/master/jquery/highlighter.min.js
 // @grant         GM_deleteValue
 // @grant         GM_getValue
 // @grant         GM_registerMenuCommand
 // @grant         GM_setValue
 // ==/UserScript==
 
+function onHighlight ($target) {
+    if ($target.css('color') === 'rgb(255, 255, 255)') {
+        $target.css('color', 'rgb(34, 34, 34)');
+    }
+}
+
 $.highlight({
-    item: 'a.title-link',
-    id: 'href',
-    target: '.title-link__title-text'
+    item:        'a.title-link',
+    id:          'href',
+    target:      '.title-link__title-text',
+    onHighlight: onHighlight,
 });
