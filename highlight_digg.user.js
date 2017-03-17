9528b27172ea847f64614bd50ee5bc29464be440
chocolateboy <chocolate@cpan.org>
2017-03-17 22:49:05 +0000

    Digg Highlighter: exclude fake news

--- highlight_digg.user.js
+++ highlight_digg.user.js
@@ -4,11 +4,11 @@
 // @author        chocolateboy
 // @copyright     chocolateboy
 // @namespace     https://github.com/chocolateboy/userscripts
-// @version       0.7.0
+// @version       0.8.0
 // @license       GPL: http://www.gnu.org/copyleft/gpl.html
 // @include       http://digg.com/
 // @include       https://digg.com/
-// @require       https://code.jquery.com/jquery-3.1.1.min.js
+// @require       https://code.jquery.com/jquery-3.2.0.min.js
 // @require       https://cdn.rawgit.com/chocolateboy/jquery-highlighter/v1.0.0/dist/highlighter.min.js
 // @grant         GM_deleteValue
 // @grant         GM_getValue
@@ -21,7 +21,9 @@ $.highlight({
     item:   function () {
         return $('article[data-content-id][data-primary-tag-slug]')
             .not('[data-primary-tag-slug="apps-we-digg"]')
+            .not('[data-primary-tag-slug="digg-picks"]')
             .not('[data-primary-tag-slug="digg-store"]')
+            .not('[data-primary-tag-slug="donaldtrump"]')
     },
     target: 'a.digg-story__title-link',
     id:     'data-content-id'
