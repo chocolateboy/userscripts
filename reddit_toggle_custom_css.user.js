6fab644ec066db2b636f331c6156ffa8c2964a62
chocolateboy <chocolate@cpan.org>
2017-05-27 14:52:48 +0100

    fix flash of styled content on subreddits with a custom background
    + disable custom theme as well as custom stylesheet (untested)
    + code cleanup + cosmetics
    + upgrade jQuery 3.1.1 -> 3.2.1

--- reddit_toggle_custom_css.user.js
+++ reddit_toggle_custom_css.user.js
@@ -1,54 +1,55 @@
 // ==UserScript==
-// @name           Reddit - Toggle Custom CSS
-// @description    Persistently disable/re-enable subreddit-specific CSS via a userscript command
-// @author         chocolateboy
-// @namespace      https://github.com/chocolateboy/userscripts
-// @include        http://reddit.com/r/*
-// @include        https://reddit.com/r/*
-// @include        http://*.reddit.com/r/*
-// @include        https://*.reddit.com/r/*
-// @require        https://code.jquery.com/jquery-3.1.1.min.js
-// @version        0.0.4
-// @run-at         document-start
-// @grant          GM_addStyle
-// @grant          GM_deleteValue
-// @grant          GM_getValue
-// @grant          GM_setValue
-// @grant          GM_registerMenuCommand
+// @name         Reddit - Toggle Custom CSS
+// @description  Persistently disable/re-enable subreddit-specific CSS via a userscript command
+// @author       chocolateboy
+// @namespace    https://github.com/chocolateboy/userscripts
+// @include      http://reddit.com/r/*
+// @include      https://reddit.com/r/*
+// @include      http://*.reddit.com/r/*
+// @include      https://*.reddit.com/r/*
+// @require      https://code.jquery.com/jquery-3.2.1.min.js
+// @version      1.0.0
+// @run-at       document-start
+// @grant        GM_addStyle
+// @grant        GM_deleteValue
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_registerMenuCommand
 // ==/UserScript==
 
-// original: http://userscripts-mirror.org/scripts/show/109818
+// inspired by: http://userscripts-mirror.org/scripts/show/109818
 
-// Why this remix? Because a) I don't use the other toggles (and the Unix philosophy
-// suggests they should be separate userscripts) and b) several subreddits mangle the
-// extra buttons so it's impossible to see/click them e.g. /r/ConTalks/
-// Also, this is much easier to understand/maintain.
-
-var SUBREDDIT = location.pathname.match(/\/r\/(\w+)/)[1];
-var CUSTOM_CSS = 'link[title="applied_subreddit_stylesheet"]';
+const SUBREDDIT = location.pathname.match(/\/r\/(\w+)/)[1]
+const CUSTOM_CSS = 'link[ref^="applied_subreddit_"]'
 
 function toggle () {
-    var disableCss = !GM_getValue(SUBREDDIT, false);
+    const oldDisableCss = GM_getValue(SUBREDDIT, false)
+    const disableCss = !oldDisableCss
 
-    $(CUSTOM_CSS).prop('disabled', disableCss);
+    $(CUSTOM_CSS).prop('disabled', disableCss)
 
     if (disableCss) {
-        GM_setValue(SUBREDDIT, true);
+        GM_setValue(SUBREDDIT, true)
     } else {
-        GM_deleteValue(SUBREDDIT);
+        GM_deleteValue(SUBREDDIT)
     }
 }
 
-var disableCss = GM_getValue(SUBREDDIT, false);
+const disableCss = GM_getValue(SUBREDDIT, false)
 
 if (disableCss) {
     // https://wiki.greasespot.net/DOMContentLoaded#Workaround
-    GM_addStyle("body { visibility: hidden }");
+
+    // NOTE we need to disable the display rather than setting its
+    // visibility to hidden as the latter doesn't hide the background
+    // (which leads to a flash of styled content on subreddits with a custom
+    // background color and/or image)
+    GM_addStyle('body { display: none !important }')
 
     $(document).on('DOMContentLoaded', function () {
         $(CUSTOM_CSS).prop('disabled', true)
-        GM_addStyle("body { visibility: visible !important }");
-    });
+        GM_addStyle('body { display: initial !important }')
+    })
 }
 
-GM_registerMenuCommand('Toggle Custom CSS', toggle);
+GM_registerMenuCommand('Toggle Custom CSS', toggle)
