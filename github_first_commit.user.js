2024c1b8ea3f370842c1d8d881eb60f248c0add4
chocolateboy <chocolate@cpan.org>
2017-11-19 22:21:49 +0000

    GitHub First Commit: use http://www.buhtig.com

--- github_first_commit.user.js
+++ github_first_commit.user.js
@@ -1,15 +1,16 @@
 // ==UserScript==
 // @name          GitHub First Commit
-// @description   Add a link to a GitHub repo's first commit via http://first-commit.com
+// @description   Add a link to a GitHub repo's first commit via http://www.buhtig.com
 // @author        chocolateboy
 // @copyright     chocolateboy
 // @namespace     https://github.com/chocolateboy/userscripts
-// @version       1.3.0
+// @version       2.0.0
 // @license       GPL: http://www.gnu.org/copyleft/gpl.html
 // @include       https://github.com/*/*
 // @require       https://code.jquery.com/jquery-3.2.1.min.js
 // @require       https://cdn.rawgit.com/eclecto/jQuery-onMutate/v1.4.2/src/jquery.onmutate.min.js
 // @grant         GM_log
+// @grant         GM_xmlhttpRequest
 // ==/UserScript==
 
 // XXX note: the unused grant is a workaround for a Greasemonkey bug:
@@ -23,18 +24,42 @@
 // with a latest-commit link
 const LATEST_COMMIT_SELECTOR = 'div.commit-tease.js-details-container > span.float-right'
 
+function getLink (repo, $link) {
+    return function (event) {
+        $link.text('Loading...')
+
+        GM_xmlhttpRequest({
+            method: 'GET',
+            url: `http://www.buhtig.com:10000/?repo=${repo}&commit=1`,
+            onload (res) {
+                const url = res.responseText.replace(/^lnk:/, '')
+                location = url
+            },
+            onerror (res) {
+                console.warn(`error (${res.status}) loading first commit for ${repo}: ${res.statusText}`)
+            }
+        })
+
+        return false
+    }
+}
+
 function addLink ($latestCommit) {
     if ($latestCommit.not(':has(#first-commit)').length) {
         const repo = $('meta[name="octolytics-dimension-repository_network_root_nwo"]')
             .attr('content')
-        const href = 'http://first-commit.com/' + repo
 
-        $latestCommit.append(
+        const $firstCommit = $(
             '<span id="first-commit">' +
                 '|&nbsp;' +
-                '<a class="message" href="' + href + '">First commit</a>' +
+                `<a id="first-commit-link" href="http://www.buhtig.com/${repo}" class="message">First commit</a>` +
             '</span>'
         )
+
+        const $link = $firstCommit.find('#first-commit-link')
+
+        $link.one('click', getLink(repo, $link))
+        $latestCommit.append($firstCommit)
     }
 }
 
