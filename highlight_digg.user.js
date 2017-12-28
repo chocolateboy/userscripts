f38c2b10ff37c58a12760cc8e5573a921ee6c037
chocolateboy <chocolate@cpan.org>
2017-12-28 03:20:09 +0000

    add exclusion

--- highlight_digg.user.js
+++ highlight_digg.user.js
@@ -4,12 +4,12 @@
 // @author        chocolateboy
 // @copyright     chocolateboy
 // @namespace     https://github.com/chocolateboy/userscripts
-// @version       0.9.1
+// @version       0.10.0
 // @license       GPL: http://www.gnu.org/copyleft/gpl.html
 // @include       http://digg.com/
 // @include       https://digg.com/
-// @require       https://code.jquery.com/jquery-3.2.0.min.js
-// @require       https://cdn.rawgit.com/chocolateboy/jquery-highlighter/v1.0.0/dist/highlighter.min.js
+// @require       https://code.jquery.com/jquery-3.2.1.min.js
+// @require       https://cdn.rawgit.com/chocolateboy/jquery-highlighter/v2.1.0/dist/highlighter.min.js
 // @grant         GM_deleteValue
 // @grant         GM_getValue
 // @grant         GM_registerMenuCommand
@@ -21,6 +21,7 @@ $.highlight({
     item: function () {
         return $('article[data-content-id][data-primary-tag-slug]')
             .not('[data-primary-tag-slug="apps-we-digg"]')
+            .not('[data-primary-tag-slug="digg-giveaways"]')
             .not('[data-primary-tag-slug="digg-picks"]')
             .not('[data-primary-tag-slug="digg-store"]')
             .not('[data-content-id="2bcqtJ7"]') // [1]
