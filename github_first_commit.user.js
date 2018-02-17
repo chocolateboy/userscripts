f4bb225586917b638d0b71b6d6ef4a91c7d4ff92
chocolateboy <chocolate@cpan.org>
2018-02-17 00:54:27 +0000

    inline the API query + fix a dependency URL to appease GreasyFork

--- github_first_commit.user.js
+++ github_first_commit.user.js
@@ -4,27 +4,16 @@
 // @author        chocolateboy
 // @copyright     chocolateboy
 // @namespace     https://github.com/chocolateboy/userscripts
-// @version       2.1.0
+// @version       2.2.0
 // @license       GPL: http://www.gnu.org/copyleft/gpl.html
 // @include       https://github.com/*/*
 // @require       https://code.jquery.com/jquery-3.3.1.min.js
-// @require       https://cdn.rawgit.com/eclecto/jQuery-onMutate/v1.4.2/src/jquery.onmutate.min.js
-// @require       https://cdn.rawgit.com/pitaj/e52862409dd5726711214a55189f332d/raw/71cec49b5f1648749d20260b496a4c92e25239c6/Get%2520the%2520first%2520commit%2520url.js
+// @require       https://cdn.rawgit.com/eclecto/jQuery-onMutate/79bbb2b8caccabfc9b9ade046fe63f15f593fef6/src/jquery.onmutate.min.js
 // @grant         GM_log
-// @grant         GM_xmlhttpRequest
 // ==/UserScript==
 
 // XXX note: the unused grant is a workaround for a Greasemonkey bug:
 // https://github.com/greasemonkey/greasemonkey/issues/1614
-
-// the included function (openFirstCommit) for extracting (and navigating to)
-// the URL from GitHub's public API comes from:
-//
-//     https://gist.github.com/pitaj/e52862409dd5726711214a55189f332d
-//
-// similar/related snippets are listed here:
-//
-//     https://github.com/wong2/first-commit/issues/15#issuecomment-317750579
 
 const COMMIT_BAR = 'div.commit-tease.js-details-container > span.float-right'
 
@@ -34,6 +23,48 @@ const FIRST_COMMIT =
         <a id="first-commit-link" style="cursor: pointer" class="message">First commit</a>
     </span>`
 
+// this function extracts (and navigates to) the URL of the repo's first-commit.
+// it is based on:
+//
+//     https://gist.github.com/pitaj/e52862409dd5726711214a55189f332d
+//
+// similar/related snippets are listed here:
+//
+//     https://github.com/wong2/first-commit/issues/15#issuecomment-317750579
+function openFirstCommit (user, repo) {
+    return fetch(`https://api.github.com/repos/${user}/${repo}/commits`)
+        // the `Link` header has additional URLs for paging.
+        // parse the original JSON for the case where no other pages exist
+        .then(res => Promise.all([res.headers.get('link'), res.json()]))
+
+        .then(([link, commits]) => {
+            if (link) {
+                // the link header contains two URLs and has the following
+                // format in a single line (wrapped for readability):
+                //
+                //     <https://api.github.com/repositories/1234/commits?page=2>;
+                //     rel="next",
+                //     <https://api.github.com/repositories/1234/commits?page=9>;
+                //     rel="last"
+
+                // extract the URL of the last page
+                const lastPage = link.match(/^.+?<([^>]+)>;/)[1]
+
+                // fetch the last page of results
+                return fetch(lastPage).then(res => res.json())
+            }
+
+            // if no link, we know we're on the only page
+            return commits
+        })
+
+        // get the last commit and extract the target URL
+        .then(commits => commits[commits.length - 1].html_url)
+
+        // navigate there
+        .then(url => location.href = url)
+}
+
 // add the "First commit" link as the last child of the commit bar
 function addLink ($commitBar) {
     const [user, repo] = $('meta[name="octolytics-dimension-repository_network_root_nwo"]')
@@ -41,9 +72,9 @@ function addLink ($commitBar) {
         .split('/')
 
     // the "First commit" link already exists when navigating to a repo's
-    // homepage via the back button. however, resurrecting the link in this
-    // way causes i"Latest commit" ts onclick event handler to be unregistered
-    // (XXX why?), so we need to re-attach it
+    // homepage via the back button. however, resurrecting the link in this way
+    // causes its onclick event handler to be unregistered (XXX why?), so we
+    // need to re-attach it
     let $firstCommit = $commitBar.find('#first-commit')
 
     if (!$firstCommit.length) {
