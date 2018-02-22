6ace04e29905ace53df89670bb64361266028940
chocolateboy <chocolate@cpan.org>
2018-02-22 05:53:16 +0000

    Echo JS Highlighter: add HTTPS

--- highlight_echo_js.user.js
+++ highlight_echo_js.user.js
@@ -4,10 +4,11 @@
 // @author        chocolateboy
 // @copyright     chocolateboy
 // @namespace     https://github.com/chocolateboy/userscripts
-// @version       0.5.0
+// @version       0.6.0
 // @license       GPL: http://www.gnu.org/copyleft/gpl.html
 // @include       http://www.echojs.com/
-// @require       https://code.jquery.com/jquery-3.1.1.min.js
+// @include       https://www.echojs.com/
+// @require       https://code.jquery.com/jquery-3.3.1.min.js
 // @require       https://cdn.rawgit.com/chocolateboy/jquery-highlighter/v1.0.0/dist/highlighter.min.js
 // @grant         GM_deleteValue
 // @grant         GM_getValue
