86e38d181eaab67af1e7006a604f82186a08912b
chocolateboy <chocolate@cpan.org>
2018-03-08 14:19:44 +0000

    GreasyForkify highlighters (cont.): fix RawGit URLs

--- highlight_reddit.user.js
+++ highlight_reddit.user.js
@@ -4,11 +4,11 @@
 // @author        chocolateboy
 // @copyright     chocolateboy
 // @namespace     https://github.com/chocolateboy/userscripts
-// @version       0.13.0
+// @version       0.13.1
 // @license       GPL: http://www.gnu.org/copyleft/gpl.html
 // @include       /^https?:\/\/([^.]+\.)?reddit\.com(\/r\/[^\/]+(/(hot|new|rising|controversial|top))?)?\/?$/
 // @require       https://code.jquery.com/jquery-3.3.1.min.js
-// @require       https://rawgit.com/chocolateboy/jquery-highlighter/478971a2a6e279f73cc65680e1e25ae0b62a3820/dist/highlighter.min.js
+// @require       https://cdn.rawgit.com/chocolateboy/jquery-highlighter/478971a2a6e279f73cc65680e1e25ae0b62a3820/dist/highlighter.min.js
 // @grant         GM_deleteValue
 // @grant         GM_getValue
 // @grant         GM_registerMenuCommand
