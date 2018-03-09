6897e605eaef32c8e4e67bbf8608c5e390dfc4c5
chocolateboy <chocolate@cpan.org>
2018-03-09 19:03:07 +0000

    Reddit Highlighter: add support for filtered subreddit pages
    e.g. https://www.reddit.com/r/popular/top/?sort=top&t=year

--- highlight_reddit.user.js
+++ highlight_reddit.user.js
@@ -7,7 +7,7 @@
 // @version       0.13.2
 // @license       GPL: http://www.gnu.org/copyleft/gpl.html
 // @include       https://*.reddit.com/
-// @include       /^https?:\/\/([^.]+\.)?reddit\.com(\/r\/[^\/]+(/(hot|new|rising|controversial|top))?)?\/?$/
+// @include       /^https://[^.]+\.reddit\.com/r/[^/]+/($|(controversial|hot|new|rising|top)/)/
 // @require       https://code.jquery.com/jquery-3.3.1.min.js
 // @require       https://cdn.rawgit.com/chocolateboy/jquery-highlighter/478971a2a6e279f73cc65680e1e25ae0b62a3820/dist/highlighter.min.js
 // @grant         GM_deleteValue
@@ -19,5 +19,5 @@
 $.highlight({
     item:   'div#siteTable div.thing[data-fullname]',
     target: 'a.title',
-    id:     'data-fullname'
-});
+    id:     'data-fullname',
+})
