8e048817578cc105d84413928321eb327c196642
chocolateboy <chocolate@cpan.org>
2018-03-24 01:19:07 +0000

    update API query

--- imdb_tomatoes.user.js
+++ imdb_tomatoes.user.js
@@ -4,7 +4,7 @@
 // @author        chocolateboy
 // @copyright     chocolateboy
 // @namespace     https://github.com/chocolateboy/userscripts
-// @version       2.4.1
+// @version       2.5.0
 // @license       GPL: http://www.gnu.org/copyleft/gpl.html
 // @include       http://*.imdb.tld/title/tt*
 // @include       http://*.imdb.tld/*/title/tt*
@@ -12,7 +12,7 @@
 // @include       https://*.imdb.tld/*/title/tt*
 // @require       https://code.jquery.com/jquery-3.3.1.min.js
 // @require       https://cdn.rawgit.com/urin/jquery.balloon.js/8b79aab63b9ae34770bfa81c9bfe30019d9a13b0/jquery.balloon.js
-// @resource      query https://git.io/vAndS
+// @resource      query https://pastebin.com/raw/MuuBJebQ
 // @grant         GM_addStyle
 // @grant         GM_deleteValue
 // @grant         GM_getResourceText
