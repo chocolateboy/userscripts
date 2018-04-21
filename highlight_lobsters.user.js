6e227124ecdb52c4469b5b21e7ef92e75eb1bf38
chocolateboy <chocolate@cpan.org>
2018-04-22 00:01:16 +0100

    Lobsters Highlighter: include topic pages

--- highlight_lobsters.user.js
+++ highlight_lobsters.user.js
@@ -4,10 +4,10 @@
 // @author        chocolateboy
 // @copyright     chocolateboy
 // @namespace     https://github.com/chocolateboy/userscripts
-// @version       0.0.1
+// @version       0.0.2
 // @license       GPL: http://www.gnu.org/copyleft/gpl.html
 // @include       https://lobste.rs/
-// @include       /^https://lobste\.rs/(newest|page|recent)\b/
+// @include       /^https://lobste\.rs/(newest|page|recent|t)\b/
 // @require       https://code.jquery.com/jquery-3.3.1.min.js
 // @require       https://cdn.rawgit.com/chocolateboy/jquery-highlighter/478971a2a6e279f73cc65680e1e25ae0b62a3820/dist/highlighter.min.js
 // @grant         GM_deleteValue
