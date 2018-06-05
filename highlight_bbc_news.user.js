558204f8afa2402de689494373eb4a805d1d4b72
chocolateboy <chocolate@cpan.org>
2018-06-05 15:43:13 +0100

    BBC News Highlighter: add HTTPS

--- highlight_bbc_news.user.js
+++ highlight_bbc_news.user.js
@@ -4,12 +4,13 @@
 // @author        chocolateboy
 // @copyright     chocolateboy
 // @namespace     https://github.com/chocolateboy/userscripts
-// @version       0.13.3
+// @version       0.13.4
 // @license       GPL: http://www.gnu.org/copyleft/gpl.html
 // @include       http://www.bbc.co.uk/news
+// @include       https://www.bbc.co.uk/news
 // @include       http://www.bbc.com/news
-// @include       /http://www\.bbc\.co\.uk/news/([^-])+$/
-// @include       /http://www\.bbc\.com/news/([^-])+$/
+// @include       https://www.bbc.com/news
+// @include       /https?://www\.bbc\.co(m|\.uk)/news/([^-])+$/
 // @require       https://code.jquery.com/jquery-3.3.1.min.js
 // @require       https://cdn.rawgit.com/eclecto/jQuery-onMutate/79bbb2b8caccabfc9b9ade046fe63f15f593fef6/src/jquery.onmutate.min.js
 // @require       https://cdn.rawgit.com/chocolateboy/jquery-highlighter/478971a2a6e279f73cc65680e1e25ae0b62a3820/dist/highlighter.min.js
