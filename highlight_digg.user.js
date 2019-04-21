b60a05b5f1440f5322112da62217ad9c2a831e5a
chocolateboy <chocolate@cpan.org>
2019-04-21 18:25:15 +0100

    Digg Highlighter:
    work around breakage in Violentmonkey when JavaScript is disabled:
    https://github.com/violentmonkey/violentmonkey/issues/302

--- highlight_digg.user.js
+++ highlight_digg.user.js
@@ -3,22 +3,23 @@
 // @description   Highlight new stories on Digg
 // @author        chocolateboy
 // @copyright     chocolateboy
-// @version       0.12.1
+// @version       0.13.0
 // @namespace     https://github.com/chocolateboy/userscripts
 // @license       GPL: http://www.gnu.org/copyleft/gpl.html
 // @include       http://digg.com/
 // @include       https://digg.com/
-// @require       https://code.jquery.com/jquery-3.3.1.min.js
+// @require       https://code.jquery.com/jquery-3.4.0.min.js
 // @require       https://cdn.jsdelivr.net/gh/chocolateboy/jquery-highlighter@478971a2a6e279f73cc65680e1e25ae0b62a3820/dist/highlighter.min.js
 // @grant         GM_deleteValue
 // @grant         GM_getValue
 // @grant         GM_registerMenuCommand
 // @grant         GM_setValue
+// @inject-into   content
 // ==/UserScript==
 
 $.highlight({
     ttl: { days: 4 },
-    item: function () {
+    item () {
         return $('article[data-content-id][data-primary-tag-slug]')
             .not('[data-content-id="2bcqtJ7"]') // [1]
             .not('[data-primary-tag-display="Gem"]')
@@ -32,7 +33,7 @@ $.highlight({
     },
     target: 'a.digg-story__title-link',
     id: 'data-content-id'
-});
+})
 
 // [1] The "Donald Trump News" channel is the only remaining
 // non-news article with a data-primary-tag-slug attribute,
