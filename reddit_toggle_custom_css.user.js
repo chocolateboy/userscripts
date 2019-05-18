7117dd7e0547624bda9f93e8605d5af2cd9d16c2
chocolateboy <chocolate@cpan.org>
2019-05-18 17:37:18 +0100

    Reddit - Toggle Custom CSS:
    - define the default disabled setting in a variable so it can easily
      be changed
    - update jQuery: 3.3.1 -> 3.4.1

--- reddit_toggle_custom_css.user.js
+++ reddit_toggle_custom_css.user.js
@@ -3,14 +3,14 @@
 // @description  Persistently disable/re-enable subreddit-specific styles via a userscript command
 // @author       chocolateboy
 // @copyright    chocolateboy
-// @version      1.1.1
+// @version      1.2.0
 // @namespace    https://github.com/chocolateboy/userscripts
 // @license      GPL: http://www.gnu.org/copyleft/gpl.html
 // @include      http://reddit.com/r/*
 // @include      https://reddit.com/r/*
 // @include      http://*.reddit.com/r/*
 // @include      https://*.reddit.com/r/*
-// @require      https://code.jquery.com/jquery-3.3.1.min.js
+// @require      https://code.jquery.com/jquery-3.4.1.min.js
 // @require      https://cdn.jsdelivr.net/gh/eclecto/jQuery-onMutate@79bbb2b8caccabfc9b9ade046fe63f15f593fef6/src/jquery.onmutate.min.js
 // @grant        GM_addStyle
 // @grant        GM_deleteValue
@@ -22,11 +22,12 @@
 
 // inspired by: http://userscripts-mirror.org/scripts/show/109818
 
-const SUBREDDIT = location.pathname.match(/\/r\/(\w+)/)[1]
 const CUSTOM_CSS = 'link[ref^="applied_subreddit_"]'
+const DISABLE_CSS = false
+const SUBREDDIT = location.pathname.match(/\/r\/(\w+)/)[1]
 
 function toggle () {
-    const oldDisableCss = GM_getValue(SUBREDDIT, false)
+    const oldDisableCss = GM_getValue(SUBREDDIT, DISABLE_CSS)
     const disableCss = !oldDisableCss
 
     $(CUSTOM_CSS).prop('disabled', disableCss)
@@ -49,7 +50,7 @@ function hidePage () {
     GM_addStyle('html { display: none !important }')
 }
 
-const disableCss = GM_getValue(SUBREDDIT, false)
+const disableCss = GM_getValue(SUBREDDIT, DISABLE_CSS)
 
 if (disableCss) {
     $(document).onCreate('head', hidePage)
