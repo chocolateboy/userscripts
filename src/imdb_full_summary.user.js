5c1394279c5dc3a46a18f36e3e25125fdf9ed7b9
chocolateboy <chocolate@cpan.org>
2019-08-16 22:30:48 +0100

    IMDb Full Summary: update to reflect latest site changes

--- src/imdb_full_summary.user.js
+++ src/imdb_full_summary.user.js
@@ -3,14 +3,14 @@
 // @description Automatically show the full plot summary on IMDb
 // @author      chocolateboy
 // @copyright   chocolateboy
-// @version     1.5.1
+// @version     1.5.2
 // @namespace   https://github.com/chocolateboy/userscripts
 // @license     GPL: http://www.gnu.org/copyleft/gpl.html
 // @include     http://*.imdb.tld/title/tt*
 // @include     http://*.imdb.tld/*/title/tt*
 // @include     https://*.imdb.tld/title/tt*
 // @include     https://*.imdb.tld/*/title/tt*
-// @require     https://code.jquery.com/jquery-3.3.1.min.js
+// @require     https://code.jquery.com/jquery-3.4.1.min.js
 // @grant       GM_log
 // ==/UserScript==
 
@@ -18,11 +18,11 @@
 // https://github.com/greasemonkey/greasemonkey/issues/1614
 
 /*
- * Tests
+ * Tests:
  *
- *     truncated:     https://www.imdb.com/title/tt0067961/
- *     refspam URL:   https://www.imdb.com/title/tt0067961/?ref_=fn_tt_tt_1
- *     not truncated: https://www.imdb.com/title/tt0062474/
+ *     - truncated movie:   https://www.imdb.com/title/tt4920360/
+ *     - truncated TV show: https://www.imdb.com/title/tt4507204/
+ *     - not truncated:     https://www.imdb.com/title/tt7131622/
  */
 
 // the truncated summary
@@ -30,7 +30,7 @@ const $summary = $('.summary_text').has('a[href*="/plotsummary"]')
 
 if ($summary.length) {
     // the full summary (usually)
-    const $storyline = $('span[itemprop="description"]')
+    const $storyline = $('#titleStoryLine > div.inline').first()
 
     if ($storyline.length) {
         $summary.html($storyline.text().trim())
