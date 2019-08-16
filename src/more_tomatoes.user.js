b5bdac3545268955159cad4a870d05fa23f67779
chocolateboy <chocolate@cpan.org>
2019-08-16 22:44:54 +0100

    More Tomatoes: update to reflect latest site changes

--- src/more_tomatoes.user.js
+++ src/more_tomatoes.user.js
@@ -3,21 +3,25 @@
 // @description Automatically show the full "Movie Info" plot synopsis on Rotten Tomatoes
 // @author      chocolateboy
 // @copyright   chocolateboy
-// @version     1.1.0
+// @version     1.2.0
 // @namespace   https://github.com/chocolateboy/userscripts
 // @license     GPL: http://www.gnu.org/copyleft/gpl.html
 // @include     http://rottentomatoes.com/m/*
 // @include     http://*.rottentomatoes.com/m/*
 // @include     https://rottentomatoes.com/m/*
 // @include     https://*.rottentomatoes.com/m/*
-// @require     https://code.jquery.com/jquery-3.3.1.min.js
+// @require     https://code.jquery.com/jquery-3.4.1.min.js
 // @grant       GM_log
 // ==/UserScript==
 
 // XXX note: the unused grant is a workaround for a Greasemonkey bug:
 // https://github.com/greasemonkey/greasemonkey/issues/1614
 
-// expand the synopsis and hide the trailing "More" link
+// Test:
+//
+//   - https://www.rottentomatoes.com/m/hobo_with_a_shotgun
+
+// expand the synopsis and hide the trailing "Show More" link
 $('#movieSynopsis')
     .removeClass('clamp clamp-6')
-    .next('.clamp-toggle-container').hide()
+    .next('.clamp-toggle__container').hide()
