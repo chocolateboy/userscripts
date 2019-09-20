aeb926f9233d541ce974dd22c8d21d73d971c8b7
chocolateboy <chocolate@cpan.org>
2019-09-20 06:30:30 +0100

    Google DWIMages - update to reflect latest site change

--- src/google_dwimages.user.js
+++ src/google_dwimages.user.js
@@ -3,59 +3,35 @@
 // @description   Direct links to images and pages on Google Images
 // @author        chocolateboy
 // @copyright     chocolateboy
-// @version       1.0.1
+// @version       1.1.0
 // @namespace     https://github.com/chocolateboy/userscripts
 // @license       GPL: http://www.gnu.org/copyleft/gpl.html
-// @include       http://www.google.tld/*tbm=isch*
-// @include       https://encrypted.google.tld/*tbm=isch*
 // @include       https://www.google.tld/*tbm=isch*
-// @require       https://code.jquery.com/jquery-3.3.1.min.js
+// @include       https://encrypted.google.tld/*tbm=isch*
+// @require       https://code.jquery.com/jquery-3.4.1.min.js
 // @require       https://cdn.jsdelivr.net/gh/eclecto/jQuery-onMutate@79bbb2b8caccabfc9b9ade046fe63f15f593fef6/src/jquery.onmutate.min.js
 // @grant         GM_log
+// @inject-into   content
 // ==/UserScript==
 
 // XXX note: the unused grant is a workaround for a Greasemonkey bug:
 // https://github.com/greasemonkey/greasemonkey/issues/1614
 
-function onImageLinks ($imageLinks) {
-    $imageLinks.each(function () {
-        const $imageLink = $(this)
-        const $container = $imageLink.closest('.rg_di')
-        const meta = JSON.parse($container.find('.rg_meta').text())
+function onLinks ($links) {
+    $links.each(function () {
+        const $link = $(this)
 
-        // remove the hook from the image-link to prevent it being hijacked
-        $imageLink.removeAttr('jsaction')
+        // remove the actions from this DIV and all its descendant elements to
+        // prevent events on these elements being intercepted
+        $link.find('*').addBack().removeAttr('jsaction')
 
-        // set the image-link's href to the image URL rather than Google's
-        // interceptor
-        $imageLink.attr('href', meta.ou)
+        // parse the JSON out of the attached metadata element
+        const meta = JSON.parse($link.find('.rg_meta').text())
 
-        // there are two layouts for image-search results:
-        //
-        // 1) new (?) (private browsing mode seems to trigger it): has a
-        // dimensions tooltip (e.g. "800 x 600") in the bottom left-hand corner
-        // of the image and a footer (link) below the image with the page name
-        // and the domain in separate DIV elements
-        //
-        // 2) legacy (?): no footer below the image, but the tooltip includes
-        // the domain name (e.g. "800 x 600 - example.com")
-        //
-        // in the first case, we only need to fix the existing link (footer)
-        // i.e. we don't need to linkify the tooltip. in the second case, we
-        // wrap the tooltip in a link and assign it the page URL
-
-        const $footerLink = $container.find('a.irc-nic')
-
-        if ($footerLink.length) {
-            // 1) set the footer link's href to the page URL
-            $footerLink.removeAttr('jsaction').attr('href', meta.ru)
-        } else {
-            // 2) wrap the tooltip in a link whose href points to the page URL
-            $imageLink.find('.rg_ilmbg').wrap(
-                $('<a></a>').attr('href', meta.ru)
-            )
-        }
+        // assign the correct URIs to the image and page links
+        $link.find('a:first').attr('href', meta.ou) // image
+        $link.find('a:last').attr('href', meta.ru)  // page
     })
 }
 
-$.onCreate('a.rg_l', onImageLinks, true /* multi */)
+$.onCreate('div[data-ri][data-ved]', onLinks, true /* multi */)
