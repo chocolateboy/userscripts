bd948222799071456edfe44b2d813ea2e1f6c5eb
chocolateboy <chocolate@cpan.org>
2019-11-03 01:08:39 +0000

    Digg Highlighter: update to reflect latest site changes

--- src/highlight_digg.user.js
+++ src/highlight_digg.user.js
@@ -3,12 +3,11 @@
 // @description   Highlight new stories on Digg
 // @author        chocolateboy
 // @copyright     chocolateboy
-// @version       0.13.1
+// @version       1.0.0
 // @namespace     https://github.com/chocolateboy/userscripts
 // @license       GPL: http://www.gnu.org/copyleft/gpl.html
-// @include       http://digg.com/
 // @include       https://digg.com/
-// @require       https://code.jquery.com/jquery-3.4.0.min.js
+// @require       https://code.jquery.com/jquery-3.4.1.min.js
 // @require       https://cdn.jsdelivr.net/gh/chocolateboy/jquery-highlighter@478971a2a6e279f73cc65680e1e25ae0b62a3820/dist/highlighter.min.js
 // @grant         GM_deleteValue
 // @grant         GM_getValue
@@ -17,27 +16,15 @@
 // @inject-into   content
 // ==/UserScript==
 
+const ITEMS = ['featured', 'vertical']
+    .map(it => `article[data-id].fp-${it}-story`)
+    .join(', ')
+
 $.highlight({
     ttl: { days: 4 },
     item () {
-        return $('article[data-content-id][data-primary-tag-slug]')
-            .not('[data-content-id="2bcqtJ7"]') // [1]
-            .not('[data-primary-tag-display="Gem"]')
-            .not('[data-primary-tag-display="Promotion"]')
-            .not('[data-primary-tag-display="Promotional"]')
-            .not('[data-primary-tag-slug="apps-we-digg"]')
-            .not('[data-primary-tag-slug="digg-giveaways"]')
-            .not('[data-primary-tag-slug="digg-pick"]')
-            .not('[data-primary-tag-slug="digg-picks"]')
-            .not('[data-primary-tag-slug="digg-picks-deprecated"]')
-            .not('[data-primary-tag-slug="digg-store"]')
+        return $(ITEMS).has('[itemprop="alternativeHeadline"][style="color:"]')
     },
-    target: 'a.digg-story__title-link',
-    id: 'data-content-id'
+    target: '[itemprop="headline"]',
+    id: 'data-id'
 })
-
-// [1] The "Donald Trump News" channel is the only remaining
-// non-news article with a data-primary-tag-slug attribute,
-// so we need something else to exclude it from highlighting.
-// we can't use the slug ("donaldtrump") as that's also used
-// for Trump-related news articles, so we use its unique ID
