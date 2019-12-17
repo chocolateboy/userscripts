31d8f980f09a637ecf78116cc1c955582faaef33
chocolateboy <chocolate@cpan.org>
2019-12-17 01:44:18 +0000

    Reddit Highlighter: highlight continuations of the homepage

--- src/highlight_reddit.user.js
+++ src/highlight_reddit.user.js
@@ -3,10 +3,11 @@
 // @description   Highlight new stories on Reddit
 // @author        chocolateboy
 // @copyright     chocolateboy
-// @version       0.14.1
+// @version       0.15.0
 // @namespace     https://github.com/chocolateboy/userscripts
 // @license       GPL: http://www.gnu.org/copyleft/gpl.html
 // @include       https://*.reddit.com/
+// @include       https://*.reddit.com/?count=*
 // @include       /^https://[^.]+\.reddit\.com/(r/[^/]+/)?($|(best|controversial|hot|new|rising|top)/)/
 // @require       https://code.jquery.com/jquery-3.3.1.min.js
 // @require       https://cdn.jsdelivr.net/gh/chocolateboy/jquery-highlighter@478971a2a6e279f73cc65680e1e25ae0b62a3820/dist/highlighter.min.js
