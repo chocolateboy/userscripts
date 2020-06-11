92568e79fa9ec5192ecc678e7083584c22cd46dd
chocolateboy <chocolate@cpan.org>
2020-06-11 05:32:19 +0100

    add Linkify Twitter Trends

--- README.md
+++ README.md
@@ -89,6 +89,7 @@ supported. Some scripts work, but most haven't been tested. <br />
 
 ### Twitter
 
+- [Linkify Twitter Trends](https://greasyfork.org/en/scripts/405103-linkify-twitter-trends) - make Twitter trends links (again)
 - [Twitter Direct](https://greasyfork.org/en/scripts/404632-twitter-direct) - remove t.co tracking links from Twitter
 
 ## Highlighters
---
+++ src/linkify-twitter-trends.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name          Linkify Twitter Trends
+// @description   Make Twitter trends links (again)
+// @author        chocolateboy
+// @copyright     chocolateboy
+// @version       0.0.1
+// @namespace     https://github.com/chocolateboy/userscripts
+// @license       GPL: http://www.gnu.org/copyleft/gpl.html
+// @include       https://twitter.com/*
+// @require       https://code.jquery.com/jquery-3.5.1.slim.min.js
+// @require       https://cdn.jsdelivr.net/gh/eclecto/jQuery-onMutate@79bbb2b8caccabfc9b9ade046fe63f15f593fef6/src/jquery.onmutate.min.js
+// @grant         GM_log
+// @inject-into   auto
+// ==/UserScript==
+
+// XXX note: the unused grant is a workaround for a Greasemonkey bug:
+// https://github.com/greasemonkey/greasemonkey/issues/1614
+
+function onTrends ($trends) {
+    for (const el of $trends) {
+        const $trend = $(el)
+        const query = encodeURIComponent('"' + $trend.text() + '"')
+        const href = `${location.origin}/search?q=${query}`
+        const $link = $('<a></a>').attr('href', href)
+        $trend.wrap($link)
+    }
+}
+
+$.onCreate('[data-testid="trend"] [dir="ltr"] > span', onTrends, true /* multi */)
