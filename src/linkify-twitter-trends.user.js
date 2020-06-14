3ba4c8127f9ac52e8ddbb7141200adf5427c2d1c
chocolateboy <chocolate@cpan.org>
2020-06-14 21:33:50 +0100

    Linkify Twitter Trends: remove embedded quotes

--- src/linkify-twitter-trends.user.js
+++ src/linkify-twitter-trends.user.js
@@ -3,7 +3,7 @@
 // @description   Make Twitter trends links (again)
 // @author        chocolateboy
 // @copyright     chocolateboy
-// @version       0.0.1
+// @version       0.0.2
 // @namespace     https://github.com/chocolateboy/userscripts
 // @license       GPL: http://www.gnu.org/copyleft/gpl.html
 // @include       https://twitter.com/*
@@ -19,7 +19,8 @@
 function onTrends ($trends) {
     for (const el of $trends) {
         const $trend = $(el)
-        const query = encodeURIComponent('"' + $trend.text() + '"')
+        const quoted = $trend.text().replace(/"/g, '')
+        const query = encodeURIComponent('"' + quoted + '"')
         const href = `${location.origin}/search?q=${query}`
         const $link = $('<a></a>').attr('href', href)
         $trend.wrap($link)
