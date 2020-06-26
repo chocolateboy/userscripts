35a008324c9b82c31cf70a9364c42a2a1f33d56d
chocolateboy <chocolate@cpan.org>
2020-06-26 02:29:42 +0100

    GitHub First Commit: update to support the latest site changes

--- src/github_first_commit.user.js
+++ src/github_first_commit.user.js
@@ -3,26 +3,22 @@
 // @description   Add a link to a GitHub repo's first commit
 // @author        chocolateboy
 // @copyright     chocolateboy
-// @version       2.5.5
+// @version       2.6.0
 // @namespace     https://github.com/chocolateboy/userscripts
 // @license       GPL: https://www.gnu.org/copyleft/gpl.html
 // @include       https://github.com/
 // @include       https://github.com/*
 // @require       https://code.jquery.com/jquery-3.5.1.slim.min.js
-// @require       https://cdn.jsdelivr.net/gh/pie6k/jquery.initialize@16342abd3d411a20d35390f3e4c966ceb37ec43e/jquery.initialize.min.js
+// @require       https://cdn.jsdelivr.net/gh/eclecto/jQuery-onMutate@79bbb2b8caccabfc9b9ade046fe63f15f593fef6/src/jquery.onmutate.min.js
 // @grant         GM_log
-// @inject-into   content
+// @inject-into   auto
 // ==/UserScript==
 
 // XXX note: the unused grant is a workaround for a Greasemonkey bug:
 // https://github.com/greasemonkey/greasemonkey/issues/1614
 
-const COMMIT_BAR = 'div.commit-tease.js-details-container'
-
-const FIRST_COMMIT =
-    `<span id="first-commit">
-        &nbsp; &#124; <a id="first-commit-link" style="cursor: pointer" class="text-inherit">First commit</a>
-     </span>`
+const COMMIT_BAR = 'div.js-details-container[data-issue-and-pr-hovercards-enabled] > *:last ul'
+const FIRST_COMMIT_LABEL = '<span aria-label="First commit"><strong>1st</strong> commit</span>'
 
 // this function extracts the URL of the repo's first commit and navigates to it.
 // it is based on code by several developers, a list of whom can be found here:
@@ -68,38 +64,65 @@ function openFirstCommit (user, repo) {
 }
 
 // add the "First commit" link as the last child of the commit bar
-function addLink () {
-    const $commitBar = $(this)
+function addLink ($commitBar) {
+    let $firstCommit = $commitBar.find('#first-commit')
+
+    if ($firstCommit.length) {
+        return
+    }
+
+    /*
+     * This is the first LI in the commit bar (UL), which we clone to create the
+     * "First commit" widget.
+     *
+     *   <li class="ml-3">
+     *     <a data-pjax="" href="/foo/bar/commits/master" class="link-gray-dark no-underline">
+     *       <svg height="16">...</svg>
+     *
+     *       <span class="d-none d-sm-inline">
+     *           <strong>42</strong>
+     *           <span aria-label="Commits on master">commits</span>
+     *       </span>
+     *     </a>
+     *   </li>
+     */
+
+    // create it
+    $firstCommit = $commitBar
+        .find('li')
+        .eq(0)
+        .clone()
+        .attr('id', 'first-commit')
+
+    const $link = $firstCommit
+        .find('a')
+        .removeAttr('href')
+        .css('cursor', 'pointer')
+
+    const $label = $(FIRST_COMMIT_LABEL)
+
+    $link.find('> span').empty().append($label)
+
     const [user, repo] = $('meta[name="octolytics-dimension-repository_network_root_nwo"]')
         .attr('content')
         .split('/')
 
-    // the "First commit" link already exists when navigating to a repo's
-    // homepage via the back button. however, resurrecting the link in this way
-    // causes its onclick event handler to be unregistered (XXX why?), so we
-    // still need to re-attach it
-    let $firstCommit = $commitBar.find('#first-commit')
+    const oldLabelHtml = $label.html()
 
-    if (!$firstCommit.length) {
-        $firstCommit = $(FIRST_COMMIT)
-        $commitBar.append($firstCommit)
-    }
+    // restore the original label so it has the correct value if we navigate
+    // back to the repo page without making a new request (e.g. via the back
+    // button)
+    $(window).on('unload', () => {
+        $label.html(oldLabelHtml)
+    })
 
-    const $link = $firstCommit.find('#first-commit-link')
-
-    $link.on('click', event => {
-        $link.text('Loading...')
+    $link.on('click', () => {
+        $label.text('Loading...')
         openFirstCommit(user, repo)
         return false
     })
+
+    $commitBar.append($firstCommit)
 }
 
-// the commit bar (div.commit-tease) is statically defined in the HTML
-// for users who aren't logged in. for logged-in users, it's loaded dynamically
-// via an <include-fragment> custom element:
-//
-//     https://github.com/github/include-fragment-element
-//
-// jquery.initialize fires the callback immediately if the element already exists,
-// so it handles both cases
-$.initialize(COMMIT_BAR, addLink)
+$.onCreate(COMMIT_BAR, addLink, true /* multi */)
