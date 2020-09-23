edf166abe44cb055b8beca0b751eaa6e1dc094fd
chocolateboy <chocolate@cpan.org>
2020-09-23 23:04:52 +0100

    GitHub My Issues: add a user (creator) filter on user pages

--- src/github-my-issues.user.js
+++ src/github-my-issues.user.js
@@ -3,29 +3,62 @@
 // @description   Add a link to issues you've contributed to on GitHub
 // @author        chocolateboy
 // @copyright     chocolateboy
-// @version       0.0.1
+// @version       0.1.0
 // @namespace     https://github.com/chocolateboy/userscripts
 // @license       GPL: http://www.gnu.org/copyleft/gpl.html
 // @include       https://github.com/
 // @include       https://github.com/*
 // @require       https://code.jquery.com/jquery-3.5.1.slim.min.js
+// @require       https://cdn.jsdelivr.net/gh/eclecto/jQuery-onMutate@79bbb2b8caccabfc9b9ade046fe63f15f593fef6/src/jquery.onmutate.min.js
 // @grant         GM_log
+// @run-at        document-start
 // @inject-into   auto
 // ==/UserScript==
 
 // XXX note: the unused grant is a workaround for a Greasemonkey bug:
 // https://github.com/greasemonkey/greasemonkey/issues/1614
 
-const user = $('meta[name="user-login"]').attr('content')
-const $issues = $('[aria-label="Global"] a[href="/issues"]')
+const ID = 'my-issues'
+const meta = (name, key = 'name') => $(`meta[${key}=${JSON.stringify(name)}]`).attr('content')
 
-if (user && $issues.length) {
-    const repo = $('meta[name="octolytics-dimension-repository_nwo"]').attr('content')
-    const query = escape(`involves:${user}`)
-    const href = repo ? `/${repo}/issues?q=${query}` : `/issues?q=${query}`
-    const $link = $issues.clone()
-        .attr({ href, 'data-hotkey': 'g I' })
-        .text('My Issues')
+function main (type) {
+    const self = meta('user-login')
+    const $issues = $('[aria-label="Global"] a[href="/issues"]')
 
-    $issues.after($link)
+    // if we're here via a pjax load, there may be an existing "My Issues" link
+    // from a previous page load: remove it
+    $(`#${ID}`).remove()
+
+    // console.warn(`XXX page (${type}):`, location.href)
+
+    if (self && $issues.length === 1) {
+        let path = '/issues', query = `involves:${self}`, prop
+
+        if (prop = meta('octolytics-dimension-repository_nwo')) { // user/repo
+            path = `/${prop}/issues`
+        } else if (prop = $('[data-pjax="#js-repo-pjax-container"]').attr('href')?.slice(1)) {
+            path = `/${prop}/issues`
+        } else if (prop = meta('profile:username', 'property')) { // user
+            query = [`user:${prop}`, query].join('+')
+        }
+
+        const href = `${path}?q=${escape(query)}`
+        const $link = $issues.clone()
+            .attr({ href, 'data-hotkey': 'g I', id: ID })
+            .text('My Issues')
+
+        $issues.after($link)
+    }
 }
+
+// navigation between pages on GitHub is a mixture of full page requests and
+// partial requests (pjax [1]). we detect the latter by detecting the
+// modification of the page's TITLE element.
+//
+// in the pjax case we need to take care not to keep adding "My Issues"
+// links.
+//
+// [1] https://github.com/defunkt/jquery-pjax
+
+main('page')
+$('html > head > title').onText(() => main('pjax'), true /* multi */)
