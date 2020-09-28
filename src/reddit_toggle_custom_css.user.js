1c519be2a723cf477a79518834789e7492593336
chocolateboy <chocolate@cpan.org>
2020-09-28 23:27:44 +0100

    Reddit - Toggle Custom CSS: fix blank pages

--- src/reddit_toggle_custom_css.user.js
+++ src/reddit_toggle_custom_css.user.js
@@ -3,16 +3,13 @@
 // @description   Persistently disable/re-enable custom subreddit styles via a userscript command
 // @author        chocolateboy
 // @copyright     chocolateboy
-// @version       1.3.1
+// @version       1.4.0
 // @namespace     https://github.com/chocolateboy/userscripts
 // @license       GPL: http://www.gnu.org/copyleft/gpl.html
 // @include       http://reddit.com/r/*
 // @include       https://reddit.com/r/*
 // @include       http://*.reddit.com/r/*
 // @include       https://*.reddit.com/r/*
-// @require       https://code.jquery.com/jquery-3.5.1.slim.min.js
-// @require       https://cdn.jsdelivr.net/gh/eclecto/jQuery-onMutate@79bbb2b8caccabfc9b9ade046fe63f15f593fef6/src/jquery.onmutate.min.js
-// @grant         GM_addStyle
 // @grant         GM_deleteValue
 // @grant         GM_getValue
 // @grant         GM_setValue
@@ -27,40 +24,52 @@ const CUSTOM_CSS = 'link[ref^="applied_subreddit_"]'
 const DISABLE_CSS = false
 const SUBREDDIT = location.pathname.match(/\/r\/(\w+)/)[1]
 
+function disableCss () {
+    // NOTE we need to disable the display rather than setting its visibility to
+    // hidden as the latter doesn't hide the background (which leads to a flash
+    // of styled content (FOSC) on subreddits with a custom background color
+    // and/or image)
+
+    // XXX hide the HTML element rather than the BODY element as the latter
+    // still results in a FOSC on some subreddits e.g. /r/firefox
+
+    // the definition of document-start varies between userscript engines and
+    // may vary for the same userscript engine across different browser engines.
+    // currently, the following userscript-engines/browsers all expose
+    // document.documentElement (in fact, they all expose document.head as well,
+    // currently, though that is not guaranteed [1] [2]):
+    //
+    // - Greasemonkey 4 [3]
+    // - Tampermonkey for Firefox
+    // - Violentmonkey for Chrome
+    // - Violentmonkey for Firefox
+    //
+    // [1] https://github.com/violentmonkey/violentmonkey/releases/tag/v2.12.8rc16
+    // [2] https://github.com/Tampermonkey/tampermonkey/issues/211#issuecomment-317116595
+    // [3] Greasemonkey isn't supported as it doesn't support GM_registerMenuCommand
+
+    document.documentElement.style.display = 'none'
+
+    document.addEventListener('DOMContentLoaded', () => {
+        document.querySelector(CUSTOM_CSS).disabled = true
+        document.documentElement.style.display = null
+    })
+}
+
 function toggle () {
-    const oldDisableCss = GM_getValue(SUBREDDIT, DISABLE_CSS)
-    const disableCss = !oldDisableCss
+    const toggled = !GM_getValue(SUBREDDIT, DISABLE_CSS)
 
-    $(CUSTOM_CSS).prop('disabled', disableCss)
+    document.querySelector(CUSTOM_CSS).disabled = toggled
 
-    if (disableCss === DISABLE_CSS) {
+    if (toggled === DISABLE_CSS) {
         GM_deleteValue(SUBREDDIT)
     } else {
-        GM_setValue(SUBREDDIT, disableCss)
+        GM_setValue(SUBREDDIT, toggled)
     }
 }
 
-// NOTE we need to disable the display rather than setting its visibility to
-// hidden as the latter doesn't hide the background (which leads to a flash of
-// styled content (FOSC) on subreddits with a custom background color and/or
-// image)
-//
-// XXX hide the HTML element rather than the BODY element as the latter still
-// results in a FOSC on some subreddits e.g. /r/firefox
-function hidePage () {
-    GM_addStyle('html { display: none !important }')
-}
-
-const disableCss = GM_getValue(SUBREDDIT, DISABLE_CSS)
-
-if (disableCss) {
-    $(document).onCreate('head', hidePage)
-
-    // https://wiki.greasespot.net/DOMContentLoaded#Workaround
-    $(document).on('DOMContentLoaded', () => {
-        $(CUSTOM_CSS).prop('disabled', true)
-        GM_addStyle('html { display: initial !important }')
-    })
+if (GM_getValue(SUBREDDIT, DISABLE_CSS)) {
+    disableCss()
 }
 
 GM_registerMenuCommand('Toggle Custom CSS', toggle)
