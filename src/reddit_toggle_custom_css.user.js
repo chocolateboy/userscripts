60515c07819b152a7c04cbd660ae1ace12f7c73e
chocolateboy <chocolate@cpan.org>
2020-11-20 01:53:43 +0000

    Reddit Toggle Custom CSS: cleanup
    don't show the menu command if there's no stylesheet to toggle

--- src/reddit_toggle_custom_css.user.js
+++ src/reddit_toggle_custom_css.user.js
@@ -3,7 +3,7 @@
 // @description   Persistently disable/re-enable custom subreddit styles via a userscript command
 // @author        chocolateboy
 // @copyright     chocolateboy
-// @version       1.4.3
+// @version       1.5.0
 // @namespace     https://github.com/chocolateboy/userscripts
 // @license       GPL: http://www.gnu.org/copyleft/gpl.html
 // @include       http://reddit.com/r/*
@@ -21,67 +21,68 @@
 // inspired by: http://userscripts-mirror.org/scripts/show/109818
 
 const CUSTOM_CSS = 'link[ref^="applied_subreddit_"]'
-const DISABLE_CSS = false
+const DEFAULT_DISABLE_CSS = false
 const SUBREDDIT = location.pathname.match(/\/r\/(\w+)/)[1]
+const DISABLE_CSS = GM_getValue(SUBREDDIT, DEFAULT_DISABLE_CSS)
 
-function disableCss () {
-    // NOTE we need to disable the display rather than setting its visibility to
-    // hidden as the latter doesn't hide the background (which leads to a flash
-    // of styled content (FOSC) on subreddits with a custom background color
-    // and/or image)
+// the definition of document-start varies across userscript engines and may
+// vary for the same userscript engine across different browsers. currently, the
+// following userscript-engines/browsers all expose document.documentElement (in
+// fact, they all expose document.head as well, though that's not guaranteed [1]
+// [2]):
+//
+//   - Greasemonkey 4 [3]
+//   - Tampermonkey for Firefox
+//   - Violentmonkey for Chrome
+//   - Violentmonkey for Firefox
+//
+// [1] https://github.com/violentmonkey/violentmonkey/releases/tag/v2.12.8rc16
+// [2] https://github.com/Tampermonkey/tampermonkey/issues/211#issuecomment-317116595
+// [3] Greasemonkey isn't supported as it doesn't support GM_registerMenuCommand
+const { style } = document.documentElement
 
-    // XXX hide the HTML element rather than the BODY element as the latter
-    // still results in a FOSC on some subreddits e.g. /r/firefox
+function toggle (customCss) {
+    const disableCss = !GM_getValue(SUBREDDIT, DEFAULT_DISABLE_CSS)
 
-    // the definition of document-start varies between userscript engines and
-    // may vary for the same userscript engine across different browsers.
-    // currently, the following userscript-engines/browsers all expose
-    // document.documentElement (in fact, they all expose document.head as well,
-    // currently, though that is not guaranteed [1] [2]):
-    //
-    // - Greasemonkey 4 [3]
-    // - Tampermonkey for Firefox
-    // - Violentmonkey for Chrome
-    // - Violentmonkey for Firefox
-    //
-    // [1] https://github.com/violentmonkey/violentmonkey/releases/tag/v2.12.8rc16
-    // [2] https://github.com/Tampermonkey/tampermonkey/issues/211#issuecomment-317116595
-    // [3] Greasemonkey isn't supported as it doesn't support GM_registerMenuCommand
+    customCss.disabled = disableCss
 
-    const { style } = document.documentElement
-
-    style.display = 'none'
-
-    document.addEventListener('DOMContentLoaded', () => {
-        const customCss = document.querySelector(CUSTOM_CSS)
-
-        // not defined on all subreddit pages, e.g.:
-        //
-        //   ✔ /r/<subreddit>/about/rules/
-        //   x /r/<subreddit>/about/moderators/
-
-        if (customCss) {
-            customCss.disabled = true
-        }
-
-        style.removeProperty('display')
-    })
-}
-
-function toggle () {
-    const disableCss = !GM_getValue(SUBREDDIT, DISABLE_CSS)
-
-    document.querySelector(CUSTOM_CSS).disabled = disableCss
-
-    if (disableCss === DISABLE_CSS) {
+    if (disableCss === DEFAULT_DISABLE_CSS) {
         GM_deleteValue(SUBREDDIT)
     } else {
         GM_setValue(SUBREDDIT, disableCss)
     }
 }
 
-if (GM_getValue(SUBREDDIT, DISABLE_CSS)) {
-    disableCss()
+if (DISABLE_CSS) {
+    // 1) hide the page 2) disable the stylesheet 3) unhide the page
+    //
+    // NOTE we need to use `display: none` rather than `visibility: hidden` as
+    // the latter doesn't hide the background (which leads to a flash of styled
+    // content (FOSC) on subreddits with a custom background color and/or image)
+    //
+    // XXX hide the HTML element rather than the BODY element as the latter
+    // still results in a FOSC on some subreddits e.g. /r/firefox
+    style.display = 'none' // 1) hide the page
 }
 
-GM_registerMenuCommand('Toggle Custom CSS', toggle)
+document.addEventListener('DOMContentLoaded', () => {
+    // the custom stylesheet (LINK element) doesn't exist on all subreddit
+    // pages, e.g.:
+    //
+    //   ✔ /r/<subreddit>/about/rules/
+    //   x /r/<subreddit>/about/moderators/
+    const customCss = document.querySelector(CUSTOM_CSS)
+
+    if (DISABLE_CSS) {
+        if (customCss) {
+            customCss.disabled = true // 2) disable the stylesheet
+        }
+
+        style.removeProperty('display') // 3) unhide the page
+    }
+
+    // don't show the toggle command if there's no custom stylesheet to toggle
+    if (customCss) {
+        GM_registerMenuCommand('Toggle Custom CSS', () => toggle(customCss))
+    }
+})
