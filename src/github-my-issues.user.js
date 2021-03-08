6729cc50664d2ac8be8b8faf967be7b2610969e7
chocolateboy <chocolate@cpan.org>
2021-03-08 09:11:52 +0000

    GitHub My Issues: docfix

--- src/github-my-issues.user.js
+++ src/github-my-issues.user.js
@@ -3,7 +3,7 @@
 // @description   Add a link to issues you've contributed to on GitHub
 // @author        chocolateboy
 // @copyright     chocolateboy
-// @version       1.1.1
+// @version       1.1.2
 // @namespace     https://github.com/chocolateboy/userscripts
 // @license       GPL: http://www.gnu.org/copyleft/gpl.html
 // @include       https://github.com/
@@ -12,36 +12,54 @@
 // @grant         GM_log
 // ==/UserScript==
 
-// value of the ID attribute for the "My Issues" link. used to identify an
-// existing link so it can be removed on pjax page loads
+/*
+ * value of the ID attribute for the "My Issues" link. used to identify an
+ * existing link so it can be removed on pjax page loads
+ */
 const ID = 'my-issues'
 
-// selector for the "Issues" link which we clone the "My Issues" link from and
-// append to
+/*
+ * selector for the "Issues" link which we clone the "My Issues" link from and
+ * append to
+ */
 const ISSUES = '[aria-label="Global"] a[href="/issues"]'
 
-// text for the "My Issues" link
+/*
+ * text for the "My Issues" link
+ */
 const MY_ISSUES = 'My Issues'
 
-// meta tag selector for the `<user>/<repo>` identifier on full pages
+/*
+ * meta-tag selector for the `<user>/<repo>` identifier on full pages
+ */
 const PAGE_REPO = 'octolytics-dimension-repository_nwo'
 
-// meta tag selector for the `/<user>/<repo>` identifier on pjax pages
+/*
+ * selector for the `/<user>/<repo>` identifier on pjax pages
+ */
 const PJAX_REPO = '[data-pjax="#js-repo-pjax-container"]'
 
-// meta tag selector for the name of the logged-in user
+/*
+ * meta-tag selector for the name of the logged-in user
+ */
 const SELF = 'user-login'
 
-// meta tag selector for the username on a profile page
+/*
+ * meta-tag selector for the username on a profile page
+ */
 const USER = 'profile:username'
 
-// helper function which extracts a value from a meta tag
+/*
+ * helper function which extracts a value from a meta tag
+ */
 function meta (name, key = 'name') {
     const quotedName = JSON.stringify(name)
     return $(`meta[${key}=${quotedName}]`).attr('content')
 }
 
-// add the "My Issues" link
+/*
+ * add the "My Issues" link
+ */
 function run () {
     const self = meta(SELF)
     const $issues = $(ISSUES)
