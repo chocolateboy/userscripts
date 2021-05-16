5c1efe328e499adad2b95c4b19193afc36c224a7
chocolateboy <chocolate@cpan.org>
2021-05-16 13:28:45 +0100

    Twitter Direct: performance improvements
    the v2 replacement engine is now no slower than v1 and up to 15% faster

--- src/twitter-direct.user.js
+++ src/twitter-direct.user.js
@@ -3,7 +3,7 @@
 // @description   Remove t.co tracking links from Twitter
 // @author        chocolateboy
 // @copyright     chocolateboy
-// @version       2.0.1
+// @version       2.1.0
 // @namespace     https://github.com/chocolateboy/userscripts
 // @license       GPL
 // @include       https://twitter.com/
@@ -24,13 +24,35 @@ const CONTENT_TYPE = /^application\/json\b/
  * document keys under which t.co URL nodes can be found when the document is a
  * plain object. not used when the document is an array.
  *
- * some populous top-level paths don't contain t.co URLs, e.g. $.timeline.
+ * some densely-populated top-level paths don't contain t.co URLs, e.g.
+ * $.timeline.
  */
 const DOCUMENT_ROOTS = [
     'data',
     'globalObjects',
     'inbox_initial_state',
     'users',
+]
+
+/*
+ * keys of "legacy" objects which URL data is known to be found in/under,
+ * e.g. we're interested in legacy.user_refs.*, legacy.retweeted_status.* and
+ * legacy.url, but not in legacy.created_at or legacy.reply_count etc.
+ *
+ * objects under the "legacy" key typically contain dozens of keys, but we only
+ * need to probe/traverse a handful to find t.co URLs.
+ *
+ * typically this reduces the number of keys to iterate in a legacy object from
+ * 30 on average (max 39) to 2 or 3
+ */
+const LEGACY_KEYS = [
+    'binding_values',
+    'entities',
+    'extended_entities',
+    'quoted_status_permalink',
+    'retweeted_status',
+    'url',
+    'user_refs',
 ]
 
 /*
@@ -42,19 +64,28 @@ const DOCUMENT_ROOTS = [
 const LOG_THRESHOLD = 1024
 
 /*
- * nodes under these keys don't contain t.co URLs so we can speed up traversal
+ * nodes under these keys never contain t.co URLs so we can speed up traversal
  * by pruning (not descending) them
  */
 const PRUNE_KEYS = new Set([
+    'advertiser_account_service_levels',
+    'card_platform',
+    'clientEventInfo',
+    'ext',
     'ext_media_color',
     'features',
+    'feedbackInfo',
     'hashtags',
     'original_info',
     'player_image_color',
     'profile_banner_extensions',
     'profile_banner_extensions_media_color',
     'profile_image_extensions',
+    'profile_image_extensions_media_color',
+    'responseObjects',
     'sizes',
+    'user_mentions',
+    'video_info',
 ])
 
 /*
@@ -62,14 +93,6 @@ const PRUNE_KEYS = new Set([
  * to keep a running total of the number of replacements in each document type
  */
 const STATS = {}
-
-/*
- * the domain intercepted links are routed through
- *
- * not all links are intercepted. exceptions include links to twitter (e.g.
- * https://twitter.com) and card URIs (e.g. card://123456)
- */
-const TRACKING_DOMAIN = 't.co'
 
 /*
  * a pattern which matches the domain(s) we expect data (JSON) to come from.
@@ -83,29 +106,30 @@ const TWITTER_API = /^(?:(?:api|mobile)\.)?twitter\.com$/
  */
 const URL_BLACKLIST = new Set([
     '/i/api/2/badge_count/badge_count.json',
+    '/i/api/graphql/articleNudgeDomains',
     '/i/api/graphql/TopicToFollowSidebar',
 ])
 
 /*
- * object keys whose corresponding values may contain t.co URLs
+ * object keys whose corresponding values may be t.co URLs
  */
 const URL_KEYS = new Set(['url', 'string_value'])
 
 /*
- * return a truthy value (a URL instance) if the supplied value is a valid
- * URL (string), falsey otherwise
+ * return a truthy value (the URL itself) if the supplied value is a valid URL
+ * (string), falsey otherwise
  */
-const checkUrl = value => {
-    let url
+const checkUrl = (function () {
+    // this is faster than using the URL constructor (in v8), which incurs
+    // the overhead of using a try/catch block
+    const urlPattern = /^https?:\/\/\w/i
 
-    if (typeof value === 'string') {
-        try {
-            url = new URL(value)
-        } catch {}
-    }
-
-    return url
-}
+    // no need to coerce the value to a string as RegExp#test does that
+    // automatically
+    //
+    // https://tc39.es/ecma262/#sec-regexp.prototype.test
+    return value => urlPattern.test(value) && value
+})()
 
 /*
  * replace the built-in XHR#send method with a custom version which swaps in our
@@ -131,10 +155,32 @@ const hookXHRSend = oldSend => {
 }
 
 /*
- * return true if the domain of the supplied URL (string) is t.co, false
- * otherwise
+ * return true if the supplied value is an array or plain object, false otherwise
  */
-const isTracked = value => checkUrl(value)?.hostname === TRACKING_DOMAIN
+const isObject = value => value && (typeof value === 'object')
+
+/*
+ * return true if the supplied value is a plain object, false otherwise
+ *
+ * only used with JSON data, so doesn't need to be foolproof
+ */
+const isPlainObject = (function () {
+    const toString = {}.toString
+    return value => toString.call(value) === '[object Object]'
+})()
+
+/*
+ * return true if the supplied value is a t.co URL (string), false otherwise
+ */
+const isTrackedUrl = (function () {
+    // this is faster (in v8) than using the URL constructor (and a try/catch
+    // block)
+    const urlPattern = /^https?:\/\/t\.co\/\w+$/
+
+    // no need to coerce the value to a string as RegExp#test does that
+    // automatically
+    return value => urlPattern.test(value)
+})()
 
 /*
  * replacement for Twitter's default handler for XHR requests. we transform the
@@ -181,6 +227,10 @@ const onResponse = (xhr, uri) => {
         return
     }
 
+    if (!isObject(data)) {
+        return
+    }
+
     const newPath = !(path in STATS)
     const count = transform(data, path)
 
@@ -213,20 +263,50 @@ const onResponse = (xhr, uri) => {
  * and update their URL nodes in place.
  */
 const replacerFor = state => /** @this {any} */ function replacer (key, value) {
+    // exclude subtrees which never contain t.co URLs
     if (PRUNE_KEYS.has(key)) {
-        return null // a terminal value to stop traversal
+        return 0 // a terminal value to stop traversal
     }
 
-    if (URL_KEYS.has(key) && isTracked(value)) {
-        const { seen, unresolved } = state
-        const expandedUrl = checkUrl(this.expanded_url || this.expanded)
+    // we only care about the "card_url" property in binding_values
+    // objects/arrays. exclude the other 24 properties
+    if (key === 'binding_values') {
+        if (Array.isArray(value)) {
+            const found = value.find(it => it?.key === 'card_url')
+            return found ? [found] : 0
+        } else if (isPlainObject(value)) {
+            return { card_url: (value.card_url || 0) }
+        }
+    }
 
-        if (expandedUrl) {
-            seen.set(value, expandedUrl)
+    // reduce the keys under this.legacy (typically around 30) to the handful we
+    // care about
+    if (key === 'legacy' && isPlainObject(value)) {
+        const filtered = {}
+
+        for (let i = 0; i < LEGACY_KEYS.length; ++i) {
+            const key = LEGACY_KEYS[i]
+
+            if (key in value) {
+                filtered[key] = value[key]
+            }
+        }
+
+        return filtered
+    }
+
+    // expand t.co URL nodes in place
+    if (URL_KEYS.has(key) && isTrackedUrl(value)) {
+        const { seen, unresolved } = state
+
+        let expandedUrl
+
+        if ((expandedUrl = seen.get(value))) {
             this[key] = expandedUrl
             ++state.count
-        } else if (seen.has(value)) {
-            this[key] = seen.get(value)
+        } else if ((expandedUrl = checkUrl(this.expanded_url || this.expanded))) {
+            seen.set(value, expandedUrl)
+            this[key] = expandedUrl
             ++state.count
         } else {
             let targets = unresolved.get(value)
@@ -239,7 +319,8 @@ const replacerFor = state => /** @this {any} */ function replacer (key, value) {
         }
     }
 
-    return value
+    // shrink terminals (don't waste space/memory in the (discarded) JSON)
+    return isObject(value) ? value : 0
 }
 
 /*
@@ -254,9 +335,10 @@ const transform = (data, path) => {
     const state = { count: 0, seen, unresolved }
     const replacer = replacerFor(state)
 
-    if (Array.isArray(data)) {
+    // [1] top-level tweet or user data (e.g. /favorites/create.json)
+    if (Array.isArray(data) || ('id_str' in data) /* [1] */) {
         JSON.stringify(data, replacer)
-    } else if (data) {
+    } else {
         for (const key of DOCUMENT_ROOTS) {
             if (key in data) {
                 JSON.stringify(data[key], replacer)
