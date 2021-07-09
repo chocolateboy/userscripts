a8fd04e68b0d836865d7eb3455a2721e3a29d251
chocolateboy <chocolate@cpan.org>
2021-07-09 01:23:26 +0100

    IMDb Full Summary - make the sure the expansion matches

--- src/imdb-full-summary.user.js
+++ src/imdb-full-summary.user.js
@@ -3,7 +3,7 @@
 // @description   Automatically show the full plot summary on IMDb
 // @author        chocolateboy
 // @copyright     chocolateboy
-// @version       2.0.0
+// @version       2.0.1
 // @namespace     https://github.com/chocolateboy/userscripts
 // @license       GPL
 // @include       https://www.imdb.com/title/tt*
@@ -19,11 +19,13 @@
 
 // the truncated summary
 const summary = document.querySelector('[data-testid="plot-xl"]')
+const truncated = summary?.firstChild?.textContent?.trim()?.slice(0, -3)
 
 // the full summary
 const storyline = document.querySelector('[data-testid="storyline-plot-summary"] > div > div')
+const fullSummary = storyline?.firstChild?.textContent?.trim()
 
-if (summary && storyline && storyline.firstChild) {
+if (fullSummary && truncated && fullSummary.length > truncated.length && fullSummary.startsWith(truncated)) {
     const init = { childList: true }
 
     const replaceSummary = (_mutations, observer) => {
