b6902ddfbaa5401de01a7587a422e89eb68c0674
chocolateboy <chocolate@cpan.org>
2021-10-23 02:27:46 +0100

    Hacker News Highlighter: update to reflect latest site changes

--- src/highlight_hacker_news.user.js
+++ src/highlight_hacker_news.user.js
@@ -3,12 +3,12 @@
 // @description   Highlight new stories on Hacker News
 // @author        chocolateboy
 // @copyright     chocolateboy
-// @version       1.0.1
+// @version       1.1.0
 // @namespace     https://github.com/chocolateboy/userscripts
-// @license       GPL: https://www.gnu.org/copyleft/gpl.html
+// @license       GPL
 // @include       https://news.ycombinator.com/
 // @include       /^https://news\.ycombinator\.com/(active|ask|best|front|newest|news|noobstories|show|shownew)\b/
-// @require       https://code.jquery.com/jquery-3.5.1.slim.min.js
+// @require       https://code.jquery.com/jquery-3.6.0.slim.min.js
 // @require       https://cdn.jsdelivr.net/gh/chocolateboy/jquery-highlighter@63adeb7dea43c47e210fd17b0589e648239e97f0/dist/highlighter.min.js
 // @grant         GM_deleteValue
 // @grant         GM_getValue
@@ -18,7 +18,6 @@
 // ==/UserScript==
 
 $.highlight({
-    item () { return $('td a[id^=up_]').closest('tr') },
-    target: 'td.title a[href].storylink',
-    id () { return $(this).find('td a[id^=up_]').attr('id').replace('up_', '') },
+    item: 'tr.athing[id]:has(> td.votelinks)',
+    target: 'td.title a.titlelink[href]',
 })
