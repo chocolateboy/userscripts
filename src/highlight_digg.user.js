0a9dd0bf204087bcfc3719d24eaddf14abf894bb
chocolateboy <chocolate@cpan.org>
2021-11-08 19:18:37 +0000

    Digg Highlighter: update to reflect latest site changes

--- src/highlight_digg.user.js
+++ src/highlight_digg.user.js
@@ -3,28 +3,46 @@
 // @description   Highlight new stories on Digg
 // @author        chocolateboy
 // @copyright     chocolateboy
-// @version       1.1.2
+// @version       1.2.0
 // @namespace     https://github.com/chocolateboy/userscripts
-// @license       GPL: https://www.gnu.org/copyleft/gpl.html
+// @license       GPL
 // @include       https://digg.com/
-// @require       https://code.jquery.com/jquery-3.5.1.slim.min.js
+// @require       https://code.jquery.com/jquery-3.6.0.slim.min.js
 // @require       https://cdn.jsdelivr.net/gh/chocolateboy/jquery-highlighter@63adeb7dea43c47e210fd17b0589e648239e97f0/dist/highlighter.min.js
 // @grant         GM_deleteValue
 // @grant         GM_getValue
 // @grant         GM_listValues
 // @grant         GM_registerMenuCommand
 // @grant         GM_setValue
-// @inject-into   auto
 // ==/UserScript==
 
-const ITEMS = ['featured', 'vertical']
-    .map(it => `article[data-id].fp-${it}-story`)
+const PROMO_CHANNELS = [
+    '/advertising',
+    'apps-we-digg',
+    'digg-pick',
+    'digg-store',
+    'gift-guides',
+    'promotion',
+]
+
+const PROMO_SELECTOR = PROMO_CHANNELS
+    .map(name => {
+        const href = name.startsWith('/') ? name : `/channel/${name}`
+        return `a[itemprop="keywords"][href="${href}"]`
+    })
     .join(', ')
+
+/**
+ * @this {JQuery<HTMLElement>}
+ */
+function isArticle () {
+    return !$(this).find(PROMO_SELECTOR).length
+}
 
 $.highlight({
     ttl: { days: 4 },
     item () {
-        return $(ITEMS).has('[itemprop="alternativeHeadline"][style="color:"]')
+        return $('article[data-id]').has('[itemprop="headline"]').filter(isArticle)
     },
     target: '[itemprop="headline"]',
     id: 'data-id'
