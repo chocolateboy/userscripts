e3cb4583ceacf7256c074d6652e0793222d0825a
chocolateboy <chocolate@cpan.org>
2022-05-12 19:53:01 +0100

    Reddit Highlighter: add support for multireddits

--- src/highlight_reddit.user.js
+++ src/highlight_reddit.user.js
@@ -3,11 +3,12 @@
 // @description   Highlight new stories on Reddit
 // @author        chocolateboy
 // @copyright     chocolateboy
-// @version       1.1.0
+// @version       1.2.0
 // @namespace     https://github.com/chocolateboy/userscripts
 // @license       GPL: https://www.gnu.org/copyleft/gpl.html
 // @include       https://*.reddit.com/
 // @include       https://*.reddit.com/?count=*
+// @include       https://*.reddit.com/user/*/m/*
 // @include       /^https://[^.]+\.reddit\.com/(r/[^/]+/)?($|(best|controversial|hot|new|rising|top)/)/
 // @require       https://code.jquery.com/jquery-3.5.1.slim.min.js
 // @require       https://cdn.jsdelivr.net/gh/chocolateboy/jquery-highlighter@63adeb7dea43c47e210fd17b0589e648239e97f0/dist/highlighter.min.js
