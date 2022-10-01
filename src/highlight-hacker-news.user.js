8e1b50e54aa6da1c45edac253fa83ae9f81cdb92
chocolateboy <chocolate@cpan.org>
2022-10-02 00:13:37 +0100

    Hacker News Highlighter - update to reflect latest site changes

--- src/highlight-hacker-news.user.js
+++ src/highlight-hacker-news.user.js
@@ -3,12 +3,12 @@
 // @description   Highlight new stories on Hacker News
 // @author        chocolateboy
 // @copyright     chocolateboy
-// @version       1.1.0
+// @version       1.2.0
 // @namespace     https://github.com/chocolateboy/userscripts
 // @license       GPL
 // @include       https://news.ycombinator.com/
 // @include       /^https://news\.ycombinator\.com/(active|ask|best|front|newest|news|noobstories|show|shownew)\b/
-// @require       https://code.jquery.com/jquery-3.6.0.slim.min.js
+// @require       https://code.jquery.com/jquery-3.6.1.slim.min.js
 // @require       https://cdn.jsdelivr.net/gh/chocolateboy/jquery-highlighter@63adeb7dea43c47e210fd17b0589e648239e97f0/dist/highlighter.min.js
 // @grant         GM_deleteValue
 // @grant         GM_getValue
@@ -19,5 +19,5 @@
 
 $.highlight({
     item: 'tr.athing[id]:has(> td.votelinks)',
-    target: 'td.title a.titlelink[href]',
+    target: 'td.title .titleline > a[href]',
 })
