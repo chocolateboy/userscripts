bfe86621fb00f4d6ec476edaedabd26f3be43601
chocolateboy <chocolate@cpan.org>
2024-05-06 21:06:25 +0100

    Google DWIMages - update to reflect latest site changes
    closes #28

--- dist/google-dwimages.user.js
+++ dist/google-dwimages.user.js
@@ -3,18 +3,12 @@
 // @description   Direct links to images and pages on Google Images
 // @author        chocolateboy
 // @copyright     chocolateboy
-// @version       2.10.0
+// @version       3.0.0
 // @namespace     https://github.com/chocolateboy/userscripts
 // @license       GPL
-// @include       https://www.google.tld/*tbm=isch*
-// @include       https://encrypted.google.tld/*tbm=isch*
-// @require       https://cdn.jsdelivr.net/npm/cash-dom@8.1.4/dist/cash.min.js
-// @require       https://unpkg.com/gm-compat@1.1.0/dist/index.iife.min.js
-// @require       https://unpkg.com/@chocolateboy/uncommonjs@3.2.1/dist/polyfill.iife.min.js
-// @require       https://unpkg.com/get-wild@3.0.2/dist/index.umd.min.js
-// @grant         GM.registerMenuCommand
-// @grant         GM.setClipboard
-// @run-at        document-start
+// @include       https://www.google.tld/search?*tbm=isch*
+// @include       https://www.google.tld/search?*udm=2*
+// @grant         none
 // ==/UserScript==
 
 // NOTE This file is generated from src/google-dwimages.user.ts and should not be edited directly.
@@ -23,111 +17,74 @@
 (() => {
   // src/google-dwimages.user.ts
   // @license       GPL
-  var CACHE = /* @__PURE__ */ new Map();
-  var EVENTS = "auxclick click contextmenu focus focusin keydown mousedown touchstart";
-  var IMAGE_METADATA = 1;
-  var IMAGE_METADATA_ENDPOINT = /\/batchexecute\?rpcids=/;
-  var INITIAL_DATA;
-  var NODE_TYPE = 0;
-  var RESULT_INDEX = 4;
-  var UNPROCESSED_RESULTS = "div[data-ri][data-ved][jsaction]";
-  function clone(data) {
-    return JSON.parse(JSON.stringify(data));
-  }
-  function hookXhrOpen(oldOpen, $container) {
-    return function open(method, url) {
-      if (isImageDataRequest(method, url)) {
-        this.addEventListener("load", () => {
-          onLoad(this, $container);
-        });
-      }
-      GMCompat.apply(this, oldOpen, arguments);
-    };
-  }
-  function isImageDataRequest(method, url) {
-    return method.toUpperCase() === "POST" && IMAGE_METADATA_ENDPOINT.test(url);
-  }
-  function mergeImageMetadata(root) {
-    const nodes = root[56] ? exports.get(clone(root[56]), "[1][0][-1][1][0].**[0][0][0]") : exports.get(clone(root[31]), "[-1][12][2]");
-    for (const node of nodes) {
-      const type = node[NODE_TYPE];
-      if (type !== IMAGE_METADATA) {
-        continue;
-      }
-      const index = node[RESULT_INDEX];
-      const imageUrl = node[1][3][0];
-      CACHE.set(index, imageUrl);
-    }
-  }
-  function onLoad(xhr, $container) {
-    let parsed;
-    try {
-      const cooked = xhr.responseText.match(/"\[[\s\S]+\](?:\\n)?"/)[0];
-      const raw = JSON.parse(cooked);
-      parsed = JSON.parse(raw);
-    } catch (e) {
-      console.error("Can't parse response:", e);
-      return;
-    }
-    try {
-      mergeImageMetadata(parsed);
-      $container.find(UNPROCESSED_RESULTS).each(onResult);
-    } catch (e) {
-      console.error("Can't merge new metadata:", e);
-    }
-  }
-  function stopPropagation(e) {
+  var EVENTS = [
+    "auxclick",
+    "click",
+    "contextmenu",
+    "focus",
+    "focusin",
+    "keydown",
+    "mousedown",
+    "touchstart"
+  ];
+  var RESULTS = ":has(> :is([data-lpage], [data-ri]))";
+  var RESULT = ":scope > :is([data-lpage], [data-ri]):not([data-status])";
+  var stopPropagation = (e) => {
     e.stopPropagation();
-  }
-  function init() {
-    const container = document.querySelector(UNPROCESSED_RESULTS)?.parentElement;
-    if (!container) {
-      throw new Error("Can't find results container");
-    }
-    const $container = $(container);
-    mergeImageMetadata(INITIAL_DATA = GMCompat.unsafeWindow.AF_initDataChunkQueue[1].data);
-    const callback = (_mutations, observer2) => {
-      const $results = $container.find(UNPROCESSED_RESULTS);
-      for (const result of $results) {
-        const index = $(result).data("ri");
-        if (CACHE.has(index)) {
-          onResult.call(result);
-        } else {
-          observer2.disconnect();
-          break;
-        }
-      }
-    };
-    const $initial = $container.find(UNPROCESSED_RESULTS);
-    const observer = new MutationObserver(callback);
-    const xhrProto = GMCompat.unsafeWindow.XMLHttpRequest.prototype;
-    $initial.each(onResult);
-    observer.observe(container, { childList: true });
-    xhrProto.open = GMCompat.export(hookXhrOpen(xhrProto.open, $container));
-  }
-  function onResult() {
-    const $result = $(this);
-    const index = $result.data("ri");
-    const imageUrl = CACHE.get(index);
-    if (!imageUrl) {
-      console.error(`Can't find image URL for result (${index})`);
+  };
+  var onImageLink = (link, result) => {
+    const { searchParams: params } = new URL(link.href);
+    const src = params.get("imgurl");
+    if (!src) {
+      console.warn("Can't find image URL in result link:", { link, params });
       return;
     }
-    $result.find("[jsaction]").add($result).each(function() {
-      $(this).removeAttr("jsaction").on(EVENTS, stopPropagation);
-    });
-    $result.find("a").eq(0).attr("href", imageUrl);
-    CACHE.delete(index);
-  }
-  function run() {
-    try {
-      init();
-    } catch (e) {
-      console.error("Initialisation error:", e);
+    const image = link.querySelector(":scope img");
+    if (!image) {
+      console.warn("Can't find image in result link:", { result, link });
+      return;
     }
-  }
-  GM.registerMenuCommand("Copy image metadata to the clipboard", () => {
-    GM.setClipboard(JSON.stringify(INITIAL_DATA));
-  });
-  document.addEventListener("readystatechange", run, { once: true });
+    link.href = src;
+    link.title = image.alt;
+    link.target = "_blank";
+    result.dataset.status = "fixed";
+    image.parentElement.replaceChild(image, image);
+  };
+  var onResult = (result) => {
+    result.dataset.status = "pending";
+    for (const event of EVENTS) {
+      result.addEventListener(event, stopPropagation);
+    }
+    const imageLink = result.querySelector(":scope a");
+    if (!imageLink) {
+      console.warn("Can't find image link in result:", result);
+      return;
+    }
+    const init = { attributeFilter: ["href"] };
+    const callback = (_mutations, observer) => {
+      observer.disconnect();
+      if (imageLink.href) {
+        return onImageLink(imageLink, result);
+      }
+      observer.observe(imageLink, init);
+    };
+    callback([], new MutationObserver(callback));
+  };
+  var run = () => {
+    const init = { childList: true };
+    const results = document.querySelector(RESULTS);
+    if (!results) {
+      console.warn("Can't find result container");
+      return;
+    }
+    const callback = (_mutations, observer) => {
+      observer.disconnect();
+      for (const result of results.querySelectorAll(RESULT)) {
+        onResult(result);
+      }
+      observer.observe(results, init);
+    };
+    callback([], new MutationObserver(callback));
+  };
+  run();
 })();
--- src/google-dwimages.user.ts
+++ src/google-dwimages.user.ts
@@ -3,245 +3,116 @@
 // @description   Direct links to images and pages on Google Images
 // @author        chocolateboy
 // @copyright     chocolateboy
-// @version       2.10.0
+// @version       3.0.0
 // @namespace     https://github.com/chocolateboy/userscripts
 // @license       GPL
-// @include       https://www.google.tld/*tbm=isch*
-// @include       https://encrypted.google.tld/*tbm=isch*
-// @require       https://cdn.jsdelivr.net/npm/cash-dom@8.1.4/dist/cash.min.js
-// @require       https://unpkg.com/gm-compat@1.1.0/dist/index.iife.min.js
-// @require       https://unpkg.com/@chocolateboy/uncommonjs@3.2.1/dist/polyfill.iife.min.js
-// @require       https://unpkg.com/get-wild@3.0.2/dist/index.umd.min.js
-// @grant         GM.registerMenuCommand
-// @grant         GM.setClipboard
-// @run-at        document-start
+// @include       https://www.google.tld/search?*tbm=isch*
+// @include       https://www.google.tld/search?*udm=2*
+// @grant         none
 // ==/UserScript==
 
-// XXX needed to appease esbuild
-export {}
-
-declare const exports: {
-    get: typeof import('get-wild').get;
-}
-
-declare global {
-    interface Window {
-        AF_initDataChunkQueue: Array<{ data: Metadata }>;
-    }
-}
-
-type Metadata = any[];
-
-// metadata cache which maps an image's 0-based index to its URL
-const CACHE = new Map<number, string>()
-
 // events to intercept (stop propagating) in result elements
-const EVENTS = 'auxclick click contextmenu focus focusin keydown mousedown touchstart'
+const EVENTS = [
+    'auxclick',
+    'click',
+    'contextmenu',
+    'focus',
+    'focusin',
+    'keydown',
+    'mousedown',
+    'touchstart'
+]
 
-// the type of image-metadata nodes; other types may be found in the tree, e.g.
-// the type of nodes containing metadata for "Related searches" widgets is 7
-const IMAGE_METADATA = 1
+// selector for the image result container
+const RESULTS = ':has(> :is([data-lpage], [data-ri]))'
 
-// a pattern which matches the endpoint for image metadata requests
-const IMAGE_METADATA_ENDPOINT = /\/batchexecute\?rpcids=/
-
-// snapshot of the image-metadata tree for diagnostics
-let INITIAL_DATA: Metadata
-
-// the first child of a node (array) contains the node's type (integer)
-const NODE_TYPE = 0
-
-// the field (index) in an image-metadata node which contains its 0-based index
-// within the list of results. this corresponds to the value of the data-ri
-// attribute
-const RESULT_INDEX = 4
-
-// selector for image result elements (DIVs) which haven't been processed
-const UNPROCESSED_RESULTS = 'div[data-ri][data-ved][jsaction]'
-
-/******************************** helper functions ****************************/
+// selector for image result elements (relative to the result container)
+const RESULT = ':scope > :is([data-lpage], [data-ri]):not([data-status])'
 
 /**
- * deep clone a JSON-serializable value
+ * event handler for result elements which prevents their click/mousedown events
+ * being intercepted
  */
-function clone <T>(data: T): T {
-    return JSON.parse(JSON.stringify(data))
-}
-
-/**
- * return a wrapper for XmlHttpRequest#open which intercepts image-metadata
- * requests and adds the results to our metadata cache
- */
-function hookXhrOpen (oldOpen: XMLHttpRequest['open'], $container: JQuery): XMLHttpRequest['open'] {
-    return function open (this: XMLHttpRequest, method: string, url: string) {
-        if (isImageDataRequest(method, url)) {
-            // a new XHR instance is created for each metadata request, so we
-            // need to register a new listener
-            this.addEventListener('load', () => {
-                onLoad(this, $container)
-            })
-        }
-
-        // delegate to the original (there's no return value)
-        GMCompat.apply(this, oldOpen, arguments)
-    }
-}
-
-/**
- * determine whether an XHR request is an image-metadata request
- */
-function isImageDataRequest (method: string, url: string): boolean {
-    return method.toUpperCase() === 'POST' && IMAGE_METADATA_ENDPOINT.test(url)
-}
-
-/**
- * extract image metadata from the full metadata tree and add it to the cache
- */
-function mergeImageMetadata (root: Metadata): void {
-    const nodes = root[56]
-        ? exports.get(clone(root[56]), '[1][0][-1][1][0].**[0][0][0]')
-        : exports.get(clone(root[31]), '[-1][12][2]')
-
-    for (const node of nodes) {
-        // the first child is the node's type (1 for image metadata)
-        const type = node[NODE_TYPE]
-
-        // other nodes are ignored, e.g. metadata for "Related searches" widgets
-        if (type !== IMAGE_METADATA) {
-            continue
-        }
-
-        // the 0-based index of the image in the list of results (data-ri)
-        const index = node[RESULT_INDEX]
-
-        // there is more metadata in the node, but for now we only need the
-        // URL
-        const imageUrl = node[1][3][0]
-
-        CACHE.set(index, imageUrl)
-    }
-}
-
-/**
- * load handler for XHR metadata requests. parse the response and add its
- * results to the metadata cache
- */
-function onLoad (xhr: XMLHttpRequest, $container: JQuery) {
-    let parsed: Metadata
-
-    try {
-        const cooked = xhr.responseText.match(/"\[[\s\S]+\](?:\\n)?"/)![0] // '"[...]\n"'
-        const raw = JSON.parse(cooked) // '[...]'
-        parsed = JSON.parse(raw) // [...]
-    } catch (e) {
-        console.error("Can't parse response:", e)
-        return
-    }
-
-    try {
-        mergeImageMetadata(parsed)
-        // process the new images
-        $container.find(UNPROCESSED_RESULTS).each(onResult)
-    } catch (e) {
-        console.error("Can't merge new metadata:", e)
-    }
-}
-
-/**
- * event handler for image links, page links and result elements which prevents
- * their click/mousedown events being intercepted
- */
-function stopPropagation (e: Event): void {
+const stopPropagation = (e: Event): void => {
     e.stopPropagation()
 }
 
-/************************************* main ************************************/
+const onImageLink = (link: HTMLAnchorElement, result: HTMLElement): void => {
+    const { searchParams: params } = new URL(link.href)
+    const src = params.get('imgurl')
 
-/**
- * extract the data for the first ≈ 100 images embedded in the page and register
- * a listener for the requests for additional data
- */
-function init (): void {
-    const container = document.querySelector(UNPROCESSED_RESULTS)?.parentElement
-
-    if (!container) {
-        throw new Error("Can't find results container")
+    if (!src) {
+        console.warn("Can't find image URL in result link:", { link, params })
+        return
     }
 
-    const $container = $(container)
+    const image = link.querySelector<HTMLImageElement>(':scope img')
 
-    mergeImageMetadata(INITIAL_DATA = GMCompat.unsafeWindow.AF_initDataChunkQueue[1].data)
+    if (!image) {
+        console.warn("Can't find image in result link:", { result, link })
+        return
+    }
 
-    // there's static data for the first ~100 images, but only the first 50 are
-    // shown initially. the next 50 are displayed lazily and then the remaining
-    // images are fetched in batches of 100. this handles images 50-99
-    const callback = (_mutations: MutationRecord[], observer: MutationObserver) => {
-        const $results = $container.find(UNPROCESSED_RESULTS)
+    link.href = src
+    link.title = image.alt
+    link.target = '_blank' // make it consistent with the page link
+    result.dataset.status = 'fixed'
 
-        for (const result of $results) {
-            const index = $(result).data('ri') // data() converts it to an integer
+    // force a reflow (once) so the updated URL is immediately visible on hover
+    // (XXX Firefox issue, not needed in Chrome)
+    image.parentElement!.replaceChild(image, image)
+}
 
-            if (CACHE.has(index)) {
-                onResult.call(result)
-            } else {
-                observer.disconnect()
-                break
-            }
+const onResult = (result: HTMLElement): void => {
+    result.dataset.status = 'pending'
+
+    // disable the link interceptors
+    for (const event of EVENTS) {
+        result.addEventListener(event, stopPropagation)
+    }
+
+    const imageLink = result.querySelector<HTMLAnchorElement>(':scope a')
+
+    if (!imageLink) {
+        console.warn("Can't find image link in result:", result)
+        return
+    }
+
+    // wait for the href
+    const init = { attributeFilter: ['href'] }
+    const callback: MutationCallback = (_mutations, observer) => {
+        observer.disconnect()
+
+        if (imageLink.href) {
+            return onImageLink(imageLink, result)
         }
+
+        observer.observe(imageLink, init)
     }
 
-    // process the initial images
-    const $initial = $container.find(UNPROCESSED_RESULTS)
-    const observer = new MutationObserver(callback)
-    const xhrProto = GMCompat.unsafeWindow.XMLHttpRequest.prototype
-
-    $initial.each(onResult) // 0-49
-    observer.observe(container, { childList: true }) // 50-99
-    xhrProto.open = GMCompat.export(hookXhrOpen(xhrProto.open, $container)) // 100+
+    callback([], new MutationObserver(callback))
 }
 
-/**
- * process an image result (DIV), assigning the image URL to its first link and
- * disabling interceptors
- *
- * used to process the initial batch of results as well as the lazily-loaded
- * updates
- */
-function onResult (this: HTMLElement): void {
-    // grab the metadata for this result
-    const $result = $(this)
-    const index = $result.data('ri') // 0-based index of the result
-    const imageUrl = CACHE.get(index)
+const run = () => {
+    const init = { childList: true }
+    const results = document.querySelector<HTMLElement>(RESULTS)
 
-    if (!imageUrl) {
-        console.error(`Can't find image URL for result (${index})`)
-        return // continue
+    if (!results) {
+        console.warn("Can't find result container")
+        return
     }
 
-    // prevent new interceptors being added to this element and its
-    // descendants and pre-empt the existing interceptors
+    const callback: MutationCallback = (_mutations, observer) => {
+        observer.disconnect()
 
-    $result.find('[jsaction]').add($result).each(function () {
-        $(this).removeAttr('jsaction').on(EVENTS, stopPropagation)
-    })
+        for (const result of results.querySelectorAll<HTMLElement>(RESULT)) {
+            onResult(result)
+        }
 
-    // assign the correct URL to the image link
-    $result.find('a').eq(0).attr('href', imageUrl)
-
-    // the URL is no longer needed: release the memory
-    CACHE.delete(index)
-}
-
-function run () {
-    try {
-        init()
-    } catch (e) {
-        console.error('Initialisation error:', e)
+        observer.observe(results, init)
     }
+
+    callback([], new MutationObserver(callback))
 }
 
-GM.registerMenuCommand('Copy image metadata to the clipboard', () => {
-    GM.setClipboard(JSON.stringify(INITIAL_DATA))
-})
-
-document.addEventListener('readystatechange', run, { once: true })
+run()
