8a28d39011e4a60c0119a567a69e4448d1e59f6d
chocolateboy <chocolate@cpan.org>
2026-02-01 05:15:52 +0000

    Google DWIMages - update to reflect latest site changes

--- dist/google-dwimages.user.js
+++ dist/google-dwimages.user.js
@@ -3,12 +3,12 @@
 // @description   Direct links to images and pages on Google Images
 // @author        chocolateboy
 // @copyright     chocolateboy
-// @version       3.1.0
+// @version       4.0.0
 // @namespace     https://github.com/chocolateboy/userscripts
 // @license       GPL
 // @include       https://www.google.tld/search?*tbm=isch*
 // @include       https://www.google.tld/search?*udm=2*
-// @grant         none
+// @require       https://unpkg.com/gm-compat@1.1.0/dist/index.iife.min.js
 // ==/UserScript==
 
 // NOTE This file is generated from src/google-dwimages.user.ts and should not be edited directly.
@@ -22,20 +22,19 @@
   var INIT = { childList: true, subtree: true };
   var done = constant(false);
   var resume = constant(true);
-  var observe = (...args) => {
-    const $ = document;
-    const [target, init, callback] = args.length === 3 ? args : args.length === 2 ? args[0] instanceof Element ? [args[0], INIT, args[1]] : [$.body, args[0], args[1]] : [$.body, INIT, args[0]];
-    const $callback = (mutations, observer2) => {
+  var observe = ((...args) => {
+    const [target, init, callback] = args.length === 3 ? args : args.length === 2 ? args[0] instanceof Element ? [args[0], INIT, args[1]] : [document.body, args[0], args[1]] : [document.body, INIT, args[0]];
+    const onMutate = (mutations, observer2) => {
       observer2.disconnect();
-      const resume2 = callback({ mutations, observer: observer2, target, init });
+      const resume2 = callback({ mutations, observer: observer2, target });
       if (resume2 !== false) {
         observer2.observe(target, init);
       }
     };
-    const observer = new MutationObserver($callback);
-    queueMicrotask(() => $callback([], observer));
+    const observer = new MutationObserver(onMutate);
+    queueMicrotask(() => onMutate([], observer));
     return observer;
-  };
+  });
 
   // src/google-dwimages.user.ts
   // @license       GPL
@@ -50,49 +49,90 @@
     "touchstart"
   ];
   var LINK_TARGET = "_blank";
-  var RESULT = ":scope > :is([data-lpage], [data-ri]):not([data-status])";
+  var RESULT = ':scope > :is([data-lpage], [data-ri]):not([data-gd-status="done"])';
   var RESULTS = ":has(> :is([data-lpage], [data-ri]))";
+  var IMAGE_DATA = /(\["[^"]+",\d+,\d+\]),[^,]+,[^,]+,"rgb\(\d+,\d+,\d+\)"/g;
+  var CACHE = /* @__PURE__ */ new Map();
+  var DATA_ID = 0;
+  var RESULT_ID = 0;
   var stopPropagation = (e) => {
     e.stopPropagation();
   };
-  var onImageLink = (link, result) => {
-    const { searchParams: params } = new URL(link.href);
-    const src = params.get("imgurl");
-    if (!src) {
-      console.warn("Can't find image URL in result link:", { result, link, params });
-      return;
+  var extractImageUrls = (text) => {
+    const imageUrls = text.matchAll(IMAGE_DATA).flatMap((it, i) => i % 2 ? [] : [JSON.parse(it[1])[0]]);
+    let count = 0;
+    for (const url of imageUrls) {
+      ++count;
+      CACHE.set(String(++DATA_ID), url);
     }
-    const image = link.querySelector(":scope img");
-    if (!image) {
-      console.warn("Can't find image in result link:", { result, link });
-      return;
+  };
+  var hookXHRSend = (oldSend) => {
+    return function send(body = null) {
+      const oldOnReadyStateChange = this.onreadystatechange;
+      this.onreadystatechange = function(event) {
+        if (this.readyState === this.DONE && this.responseURL && this.status === 200) {
+          onResponse(this, this.responseURL);
+        }
+        if (oldOnReadyStateChange) {
+          oldOnReadyStateChange.call(this, event);
+        }
+      };
+      oldSend.call(this, body);
+    };
+  };
+  var onResponse = (xhr, uri) => {
+    if (URL.parse(uri)?.pathname === "/search") {
+      extractImageUrls(xhr.responseText.replaceAll("\\", ""));
     }
-    link.href = src;
-    link.title = image.alt;
-    link.target = LINK_TARGET;
-    result.dataset.status = "fixed" /* FIXED */;
-    image.parentElement.innerHTML = image.parentElement.innerHTML;
   };
   var onResult = (result) => {
-    result.dataset.status = "pending" /* PENDING */;
-    for (const event of EVENTS) {
-      result.addEventListener(event, stopPropagation);
+    result.dataset.gdStatus ||= "pending";
+    let id = result.dataset.gdRi;
+    if (!id) {
+      id = result.dataset.gdRi = String(++RESULT_ID);
     }
     const imageLink = result.querySelector(":scope a");
     if (!imageLink) {
-      console.warn("Can't find image link in result:", result);
+      console.warn("can't find image link in result:", result);
       return;
     }
-    observe(imageLink, { attributeFilter: ["href"] }, () => {
-      return imageLink.href && done(onImageLink(imageLink, result));
+    const image = imageLink.querySelector(":scope img");
+    if (!image) {
+      console.warn("can't find image in result link:", { result, link: imageLink });
+      return;
+    }
+    const href = CACHE.get(id);
+    if (!href) {
+      console.debug(`can't find URL for result ${id}`);
+      return;
+    }
+    for (const event of EVENTS) {
+      result.addEventListener(event, stopPropagation);
+    }
+    Object.assign(imageLink, {
+      href,
+      title: image.alt,
+      target: LINK_TARGET
+      // make it consistent with the page link
     });
+    CACHE.delete(id);
+    result.dataset.gdStatus = "done";
   };
   var run = () => {
+    const xhrProto = GMCompat.unsafeWindow.XMLHttpRequest.prototype;
+    const send = hookXHRSend(xhrProto.send);
+    xhrProto.send = GMCompat.export(send);
     const results = document.querySelector(RESULTS);
     if (!results) {
-      console.warn("Can't find result container");
+      console.warn("can't find result container");
       return;
     }
+    const script = [...document.scripts].filter((it) => !it.src).findLast((it) => it.textContent.match(IMAGE_DATA));
+    if (!script) {
+      console.warn("can't find initial data");
+      return;
+    }
+    extractImageUrls(script.textContent);
     observe(results, { childList: true }, () => {
       results.querySelectorAll(RESULT).forEach(onResult);
     });
--- src/google-dwimages.user.ts
+++ src/google-dwimages.user.ts
@@ -3,22 +3,15 @@
 // @description   Direct links to images and pages on Google Images
 // @author        chocolateboy
 // @copyright     chocolateboy
-// @version       3.1.0
+// @version       4.0.0
 // @namespace     https://github.com/chocolateboy/userscripts
 // @license       GPL
 // @include       https://www.google.tld/search?*tbm=isch*
 // @include       https://www.google.tld/search?*udm=2*
-// @grant         none
+// @require       https://unpkg.com/gm-compat@1.1.0/dist/index.iife.min.js
 // ==/UserScript==
 
-import { done, observe } from './lib/observer'
-
-// property (data attribute) on image result elements used to distinguish them
-// from new/unprocessed results
-const enum ResultStatus {
-    PENDING = 'pending',
-    FIXED   = 'fixed',
-}
+import { observe } from './lib/observer'
 
 // events to intercept (stop propagating) in result elements
 const EVENTS = [
@@ -32,14 +25,27 @@ const EVENTS = [
     'touchstart'
 ]
 
-// the default target for image links (same as page links)
+// the default target for image links (the same as page links)
 const LINK_TARGET = '_blank'
 
 // selector for image result elements (relative to the result container)
-const RESULT = ':scope > :is([data-lpage], [data-ri]):not([data-status])'
+const RESULT = ':scope > :is([data-lpage], [data-ri]):not([data-gd-status="done"])'
 
 // selector for the image result container
 const RESULTS = ':has(> :is([data-lpage], [data-ri]))'
+
+// the pattern used to scrape image URLs out of the page/pulled payloads
+const IMAGE_DATA = /(\["[^"]+",\d+,\d+\]),[^,]+,[^,]+,"rgb\(\d+,\d+,\d+\)"/g
+
+// an index used to store a result's image URL until the element is displayed.
+// keyed on the element's (1-based) index in the result list
+let CACHE = new Map<string, string>()
+
+// the index for each extracted image URL (1-based)
+let DATA_ID = 0
+
+// the index for each rendered result element (1-based)
+let RESULT_ID = 0
 
 /**
  * event handler for result elements which prevents their click/mousedown events
@@ -50,82 +56,145 @@ const stopPropagation = (e: Event): void => {
 }
 
 /*
- * process an image link:
- *
- * 1) extract the image URL from the tracking URL and assign it, e.g.
- *
- *    from: href="/imgres?imgurl=https%3A%2F%2Fexample.com%2Fimage.jpg&..."
- *      to: href="https://example.com/image.jpg"
- *
- * 2) update the result element's status to indicate it has been processed
+ * extract (scrape) image URLs from the page (initial) or from an XHR request
+ * for more. add each one to a cache to be looked up later when the result
+ * element appears
  */
-const onImageLink = (link: HTMLAnchorElement, result: HTMLElement): void => {
-    const { searchParams: params } = new URL(link.href)
-    const src = params.get('imgurl')
+const extractImageUrls = (text: string): void => {
+    // console.debug('extracting image URLs...')
 
-    if (!src) {
-        console.warn("Can't find image URL in result link:", { result, link, params })
-        return
+    // deduplicate and extract the URLs (they come in pairs)
+    const imageUrls = text
+        .matchAll(IMAGE_DATA)
+        .flatMap((it, i) => i % 2 ? [] : [JSON.parse(it[1])[0]])
+
+    let count = 0
+
+    for (const url of imageUrls) {
+        ++count
+        CACHE.set(String(++DATA_ID), url)
     }
 
-    const image = link.querySelector<HTMLImageElement>(':scope img')
+    // console.debug(`found ${count} image URLs`)
+}
 
-    if (!image) {
-        console.warn("Can't find image in result link:", { result, link })
-        return
+/*
+ * replace the built-in XHR#send method with a custom version which swaps
+ * in our custom response handler. once done, we delegate to the original
+ * handler (this.onreadystatechange)
+ */
+const hookXHRSend = (oldSend: XMLHttpRequest['send']): XMLHttpRequest['send'] => {
+    return function send (this: XMLHttpRequest, body = null) {
+        const oldOnReadyStateChange = this.onreadystatechange
+
+        this.onreadystatechange = function (event) {
+            if (this.readyState === this.DONE && this.responseURL && this.status === 200) {
+                onResponse(this, this.responseURL)
+            }
+
+            if (oldOnReadyStateChange) {
+                oldOnReadyStateChange.call(this, event)
+            }
+        }
+
+        oldSend.call(this, body)
     }
+}
 
-    link.href = src
-    link.title = image.alt
-    link.target = LINK_TARGET // make it consistent with the page link
-    result.dataset.status = ResultStatus.FIXED
-
-    // force a reflow (once) so the updated URL is immediately visible on hover
-    // (XXX Firefox issue, not needed in Chrome)
-    //
-    // this no longer works (parent is a custom element (g-img)):
-    //
-    //   image.parentElement!.replaceChild(image, image)
-    image.parentElement!.innerHTML = image.parentElement!.innerHTML
+/*
+ * replacement for the default handler for XHR requests. intercept the response
+ * if it includes the image URL data and add it to the cache
+ */
+const onResponse = (xhr: XMLHttpRequest, uri: string): void => {
+    if (URL.parse(uri)?.pathname === '/search') {
+        extractImageUrls(xhr.responseText.replaceAll('\\', ''))
+    }
 }
 
 /*
  * process an image result:
  *
  * 1) prevent its click event(s) being intercepted to open the sidebar
- * 2) wait for its image link to be assigned a URL (on the first hover) and
- *    extract the direct image URL from it
+ * 2) assign the stored image URL if available, otherwise wait for the next
+ *    DOM update
  */
 const onResult = (result: HTMLElement): void => {
-    // tag the result so we don't process it again
-    result.dataset.status = ResultStatus.PENDING
+    result.dataset.gdStatus ||= 'pending'
 
-    // disable the click interceptors
-    for (const event of EVENTS) {
-        result.addEventListener(event, stopPropagation)
+    // initialize the element's result index, e.g, 'data-gd-ri="1"'
+    let id = result.dataset.gdRi
+
+    if (!id) {
+        id = result.dataset.gdRi = String(++RESULT_ID)
     }
 
     // grab the link to the image (first link)
     const imageLink = result.querySelector<HTMLAnchorElement>(':scope a')
 
     if (!imageLink) {
-        console.warn("Can't find image link in result:", result)
+        console.warn("can't find image link in result:", result)
         return
     }
 
-    // wait for its href to be assigned (on the first hover)
-    observe(imageLink, { attributeFilter: ['href'] }, () => {
-        return imageLink.href && done(onImageLink(imageLink, result))
+    const image = imageLink.querySelector<HTMLImageElement>(':scope img')
+
+    if (!image) {
+        console.warn("can't find image in result link:", { result, link: imageLink })
+        return
+    }
+
+    const href = CACHE.get(id)
+
+    if (!href) {
+        console.debug(`can't find URL for result ${id}`)
+        return
+    }
+
+    // console.debug(`Found URL for result ${id}: ${href}`)
+
+    // disable the click interceptors
+    for (const event of EVENTS) {
+        result.addEventListener(event, stopPropagation)
+    }
+
+    Object.assign(imageLink, {
+        href,
+        title: image.alt,
+        target: LINK_TARGET, // make it consistent with the page link
     })
+
+    CACHE.delete(id)
+
+    // tag the result so we don't process it again
+    result.dataset.gdStatus = 'done'
 }
 
 const run = () => {
+    // replace the default XHR#send method with our custom version, which scans
+    // the response for image URLs and adds them to the cache
+    const xhrProto = GMCompat.unsafeWindow.XMLHttpRequest.prototype
+    const send = hookXHRSend(xhrProto.send)
+
+    xhrProto.send = GMCompat.export(send)
+
     const results = document.querySelector<HTMLElement>(RESULTS)
 
     if (!results) {
-        console.warn("Can't find result container")
+        console.warn("can't find result container")
         return
     }
+
+    // the first 100 results are embedded in the page
+    const script = [...document.scripts]
+        .filter(it => !it.src)
+        .findLast(it => it.textContent.match(IMAGE_DATA))
+
+    if (!script) {
+        console.warn("can't find initial data")
+        return
+    }
+
+    extractImageUrls(script.textContent)
 
     observe(results, { childList: true }, () => {
         results.querySelectorAll<HTMLElement>(RESULT).forEach(onResult)
